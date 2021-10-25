7d9e79615a747f9bfee16765d0da6c53aa8c39a2
Chris Birkbeck <c.birkbeck@ucl.ac.uk>
2021-10-25 22:19:46 +0100

    quick hack to make try and make workflow work

--- src/number_theory/cyclotomic/class_group.lean
+++ src/number_theory/cyclotomic/class_group.lean
@@ -1,8 +1,12 @@
-import number_theory.cyclotomic.basic
+import number_theory.cyclotomic.ring.basic
 
 variable (n : ℕ)
 
 section finiteness
+
+instance a (n : ℕ) [fact (0 < n)] : is_fraction_ring (cyclotomic_ring n) (cyclotomic_field n) := sorry
+
+instance b (n : ℕ) [fact (0 < n)] : is_integral_closure (cyclotomic_ring n) ℤ  (cyclotomic_field n) := sorry
 
 noncomputable instance class_group.fintype_of_cyclotomic_ring (n : ℕ) [fact (0 < n)] :
   fintype (class_group (cyclotomic_ring n) (cyclotomic_field n)) :=
