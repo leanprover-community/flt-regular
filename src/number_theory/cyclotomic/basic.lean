09e3f262ad5acffe7bf50b8ec3e9848b65fa3e9d
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-10-26 17:32:44 +0200

    add stuff

--- src/number_theory/cyclotomic/basic.lean
+++ src/number_theory/cyclotomic/basic.lean
@@ -17,17 +17,13 @@ end basic
 
 section field
 
-variables (K L : Type*) [field K] [field L] [algebra K L]
+variables (n : ℕ+) (S : set ℕ+) (K L : Type*) [field K] [field L] [algebra K L]
 
-section singleton
+section fintype
 
-variables (n : ℕ+) [is_cyclotomic_extension {n} K L]
+variables [fintype S] [is_cyclotomic_extension S K L]
 
 namespace is_cyclotomic_extension
-
-instance splitting_field_X_pow_sub_one : is_splitting_field K L (X ^ (n : ℕ) - 1) := sorry
-
-instance splitting_field_cyclotomic : is_splitting_field K L (cyclotomic n K) := sorry
 
 instance finite_dimensional : finite_dimensional K L := sorry
 
@@ -35,20 +31,46 @@ instance number_field [number_field K] : number_field L := sorry
 
 end is_cyclotomic_extension
 
-end singleton
-
-end field
-
-section rational
-
-variables (L : Type*) [field L] [char_zero L]
+end fintype
 
 section singleton
 
-variables (n : ℕ+) [is_cyclotomic_extension {n} ℚ L]
+variables [is_cyclotomic_extension {n} K L]
+
+namespace is_cyclotomic_extension
+
+instance splitting_field_X_pow_sub_one : is_splitting_field K L (X ^ (n : ℕ) - 1) := sorry
+
+instance splitting_field_cyclotomic : is_splitting_field K L (cyclotomic n K) := sorry
+
+end is_cyclotomic_extension
+
+end singleton
+
+section rational
+
+variables [char_zero L]
+
+section singleton
+
+variables [is_cyclotomic_extension {n} ℚ L]
 
 lemma degree : finrank ℚ L = (n : ℕ).totient := sorry
 
 end singleton
 
 end rational
+
+section cyclotomic_field
+
+def cyclotomic_field : Type* := (cyclotomic n K).splitting_field
+
+instance : field (cyclotomic_field n K) := sorry
+
+instance : algebra K (cyclotomic_field n K) := sorry
+
+instance : is_cyclotomic_extension {n} K (cyclotomic_field n K) := sorry
+
+end cyclotomic_field
+
+end field
