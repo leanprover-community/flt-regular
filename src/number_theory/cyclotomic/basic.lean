68349d38ca4ad8fdcde15cfcaa2a677b7f9f53a9
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-10-26 18:07:24 +0200

    add a lot of instances

--- src/number_theory/cyclotomic/basic.lean
+++ src/number_theory/cyclotomic/basic.lean
@@ -3,21 +3,23 @@ import number_theory.number_field
 
 open polynomial algebra finite_dimensional
 
+variables (n : ℕ+) (S : set ℕ+) (A B K L : Type*)
+variables [comm_ring A] [comm_ring B] [algebra A B]
+variables [field K] [field L] [algebra K L]
+
+noncomputable theory
+
 section basic
 
-variables (S : set ℕ+) (K L : Type*) [comm_ring K] [comm_ring L] [algebra K L]
-
 class is_cyclotomic_extension :=
-( ex_root (a : ℕ+) (ha : a ∈ S) : ∃ r : L, aeval r (cyclotomic a K) = 0 )
-( adjoint_roots : ∀ x, x ∈ adjoin K { b : L | ∃ a : ℕ+, a ∈ S ∧ b ^ (a : ℕ) = 1 } )
+( ex_root (a : ℕ+) (ha : a ∈ S) : ∃ r : B, aeval r (cyclotomic a A) = 0 )
+( adjoint_roots : ∀ x, x ∈ adjoin A { b : B | ∃ a : ℕ+, a ∈ S ∧ b ^ (a : ℕ) = 1 } )
 
 --TODO: add equivalent definitions
 
 end basic
 
 section field
-
-variables (n : ℕ+) (S : set ℕ+) (K L : Type*) [field K] [field L] [algebra K L]
 
 section fintype
 
@@ -74,3 +76,39 @@ instance : is_cyclotomic_extension {n} K (cyclotomic_field n K) := sorry
 end cyclotomic_field
 
 end field
+
+section is_domain
+
+variables [is_domain A] [algebra A K] [is_fraction_ring A K]
+
+section cyclotomic_ring
+
+instance cyclotomic_field_algebra : algebra A (cyclotomic_field n K) := sorry
+
+def cyclotomic_ring : Type* := adjoin A { b : K | b ^ (n : ℕ) = 1 }
+
+lemma cyclotomic_ring_eq_adjoin_single (μ : K) (h : μ ∈ primitive_roots n K) :
+  cyclotomic_ring n A K = adjoin A ({μ} : set K) := sorry
+
+instance : comm_ring (cyclotomic_ring n A K) := sorry
+
+instance : algebra A (cyclotomic_ring n A K) := sorry
+
+instance algebra_cycl_ring_field : algebra (cyclotomic_ring n A K) (cyclotomic_field n K) := sorry
+
+instance : is_scalar_tower A (cyclotomic_ring n A K) (cyclotomic_field n K) := sorry
+
+instance : is_cyclotomic_extension {n} A (cyclotomic_ring n A K) := sorry
+
+instance : is_fraction_ring (cyclotomic_ring n A K) (cyclotomic_field n K) := sorry
+
+end cyclotomic_ring
+
+section integers
+
+lemma cyclotomic_ring_int_is_integral_closure :
+  is_integral_closure (cyclotomic_ring n ℤ ℚ) ℤ (cyclotomic_field n ℚ) := sorry
+
+end integers
+
+end is_domain
