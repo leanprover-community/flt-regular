edc3f659f7745c9dec4858f40bece9aebc375412
CBirkbeck <c.birkbeck@ucl.ac.uk>
2021-10-26 11:57:50 +0100

    tex updates and new thm

--- src/flt_regular.lean
+++ src/flt_regular.lean
@@ -1,5 +1,10 @@
 import ring_theory.class_group
 import number_theory.regular_primes
 
+
+
+theorem flt_regular_case_one (p a b c : ℕ) [fact p.prime] (hp : is_regular_number p)
+  (h : a ^ p + b ^ p = c ^ p) : p ∣  a*b*c := sorry
+
 theorem flt_regular (p a b c : ℕ) [fact p.prime] (hp : is_regular_number p)
   (h : a ^ p + b ^ p = c ^ p) : a * b * c = 0 := sorry
