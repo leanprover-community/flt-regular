4c98a8519c793c5a7e8dc573996bad34d7a89f31
Alex J. Best <alex.j.best@gmail.com>
2021-10-26 23:06:58 +0200

    split into cases 1 and 2

--- src/flt_regular.lean
+++ src/flt_regular.lean
@@ -4,7 +4,17 @@ import number_theory.regular_primes
 
 
 theorem flt_regular_case_one (p a b c : ℕ) [fact p.prime] (hp : is_regular_number p)
-  (h : a ^ p + b ^ p = c ^ p) : p ∣  a * b * c := sorry
+  (h : a ^ p + b ^ p = c ^ p) (hpabc : p.coprime (a * b * c)) : a * b * c = 0 := sorry
+
+theorem flt_regular_case_two (p a b c : ℕ) [fact p.prime] (hp : is_regular_number p)
+  (h : a ^ p + b ^ p = c ^ p) (hpabc : p ∣ a * b * c) : a * b * c = 0 := sorry
 
 theorem flt_regular (p a b c : ℕ) [fact p.prime] (hp : is_regular_number p)
-  (h : a ^ p + b ^ p = c ^ p) : a * b * c = 0 := sorry
+  (h : a ^ p + b ^ p = c ^ p) : a * b * c = 0 :=
+begin
+  by_cases hpabc : p ∣ a * b * c,
+  exact flt_regular_case_two p a b c hp h hpabc,
+  have : p.coprime (a * b * c),
+  refine (nat.prime.coprime_iff_not_dvd (fact.out _)).mpr hpabc,
+  exact flt_regular_case_one p a b c hp h this,
+end
