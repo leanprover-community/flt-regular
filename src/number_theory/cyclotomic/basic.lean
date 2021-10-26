96d054259a086c08f1e34d226971f1171f1adf85
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-10-26 16:53:25 +0200

    add instances

--- src/number_theory/cyclotomic/basic.lean
+++ src/number_theory/cyclotomic/basic.lean
@@ -1,7 +1,59 @@
 import ring_theory.polynomial.cyclotomic
+import number_theory.number_field
 
-variables (K L : Type*) [comm_ring K] [comm_ring L] [algebra K L]
+open polynomial algebra finite_dimensional
 
-class is_cyclotomic_extension (S : set ℕ+) :=
-( ex_root (a : ℕ+) (ha : a ∈ S) : ∃ r : L, polynomial.aeval r (polynomial.cyclotomic a K) = 0 )
-( adjoint_roots : ∀ x, x ∈ algebra.adjoin K { b : L | ∃ a : ℕ+, a ∈ S ∧ b ^ (a : ℕ) = 1 } )
+section basic
+
+--PR this
+instance polynomial.cyclotomic_rat.irreducible (n : ℕ+) : irreducible (cyclotomic n ℚ) :=
+by { simpa using ((is_primitive.int.irreducible_iff_irreducible_map_cast
+    (monic.is_primitive (cyclotomic.monic n ℤ))).1 (cyclotomic.irreducible n.pos)) }
+
+variables (S : set ℕ+) (K L : Type*) [comm_ring K] [comm_ring L] [algebra K L]
+
+class is_cyclotomic_extension :=
+( ex_root (a : ℕ+) (ha : a ∈ S) : ∃ r : L, aeval r (cyclotomic a K) = 0 )
+( adjoint_roots : ∀ x, x ∈ adjoin K { b : L | ∃ a : ℕ+, a ∈ S ∧ b ^ (a : ℕ) = 1 } )
+
+--TODO: add equivalent definitions
+
+end basic
+
+section field
+
+variables (K L : Type*) [field K] [field L] [algebra K L]
+
+section singleton
+
+variables (n : ℕ+) [is_cyclotomic_extension {n} K L]
+
+namespace is_cyclotomic_extension
+
+instance splitting_field_X_pow_sub_one : is_splitting_field K L (X ^ (n : ℕ) - 1) := sorry
+
+instance splitting_field_cyclotomic : is_splitting_field K L (cyclotomic n K) := sorry
+
+instance finite_dimensional : finite_dimensional K L := sorry
+
+instance number_field [number_field K] : number_field L := sorry
+
+end is_cyclotomic_extension
+
+end singleton
+
+end field
+
+section rational
+
+variables (L : Type*) [field L] [char_zero L]
+
+section singleton
+
+variables (n : ℕ+) [is_cyclotomic_extension {n} ℚ L]
+
+lemma degree : finrank ℚ L = (n : ℕ).totient := sorry
+
+end singleton
+
+end rational
