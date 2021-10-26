3ad0e4e1907890f0ea99a5a89a7b495447d59502
CBirkbeck <c.birkbeck@ucl.ac.uk>
2021-10-26 11:58:27 +0100

    fix

--- src/flt_regular.lean
+++ src/flt_regular.lean
@@ -4,7 +4,7 @@ import number_theory.regular_primes
 
 
 theorem flt_regular_case_one (p a b c : ℕ) [fact p.prime] (hp : is_regular_number p)
-  (h : a ^ p + b ^ p = c ^ p) : p ∣  a*b*c := sorry
+  (h : a ^ p + b ^ p = c ^ p) : p ∣  a * b * c := sorry
 
 theorem flt_regular (p a b c : ℕ) [fact p.prime] (hp : is_regular_number p)
   (h : a ^ p + b ^ p = c ^ p) : a * b * c = 0 := sorry
