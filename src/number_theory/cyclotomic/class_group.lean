dc582c161f5a7408c69d04a84e8a3edbd9133c7c
CBirkbeck <c.birkbeck@ucl.ac.uk>
2021-10-27 09:42:43 +0100

    error code

--- src/number_theory/cyclotomic/class_group.lean
+++ src/number_theory/cyclotomic/class_group.lean
@@ -1,16 +1,18 @@
-import number_theory.cyclotomic.ring.basic
+import number_theory.cyclotomic.basic
+import number_theory.class_number.finite
 
-variable (n : ℕ)
+variable (n : ℕ+)
 
 section finiteness
 
-instance a (n : ℕ) [fact (0 < n)] : is_fraction_ring (cyclotomic_ring n) (cyclotomic_field n) := sorry
 
-instance b (n : ℕ) [fact (0 < n)] : is_integral_closure (cyclotomic_ring n) ℤ  (cyclotomic_field n) := sorry
 
-noncomputable instance class_group.fintype_of_cyclotomic_ring (n : ℕ) [fact (0 < n)] :
-  fintype (class_group (cyclotomic_ring n) (cyclotomic_field n)) :=
-class_group.fintype_of_admissible_of_finite ℚ (cyclotomic_field n)
+
+instance a  : is_domain  (cyclotomic_ring n ℤ ℚ ) := sorry
+
+noncomputable instance class_group.fintype_of_cyclotomic_ring (n : ℕ+) :
+  fintype (class_group (cyclotomic_ring n ℤ ℚ) (cyclotomic_field n ℚ)) :=
+class_group.fintype_of_admissible_of_finite ℚ (cyclotomic_field n ℚ)
   (absolute_value.abs_is_admissible)
 
 end finiteness
