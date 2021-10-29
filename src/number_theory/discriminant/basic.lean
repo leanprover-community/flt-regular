5f3e9d9ea443c8cee114a448936895bf92b2b74f
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-10-29 17:19:46 +0200

    add doc

--- src/number_theory/discriminant/basic.lean
+++ src/number_theory/discriminant/basic.lean
@@ -3,17 +3,21 @@ import ring_theory.trace
 
 universes u v w
 
-variables (R : Type u) {S : Type v} {ι : Type w} [fintype ι] (b : ι → S)
+variables (A : Type u) {B : Type v} {ι : Type w} [fintype ι] (b : ι → B)
+variables [comm_ring A] [comm_ring B] [algebra A B]
 
 open_locale classical
 noncomputable theory
 
 namespace algebra
 
-variables [comm_ring R] [comm_ring S] [algebra R S]
+/-- Given an `A`-algebra `B` and `b`, an `ι`-indexed family of elements of `B`, we define
+`trace_matrix A ι b` as the matrix whose `(i j)`-th element is the trace of `b i * b j`. -/
+@[nolint unused_arguments]
+def trace_matrix : matrix ι ι A := (λ i j, algebra.trace A B (b i * b j))
 
-def trace_matrix : matrix ι ι R := (λ i j, algebra.trace R S (b i * b j))
-
-def discriminant := (trace_matrix R b).det
+/-- Given an `A`-algebra `B` and `b`, an `ι`-indexed family of elements of `B`, we define
+`discriminant A ι b` as the determinant of `trace_matrix A ι b`. -/
+def discriminant := (trace_matrix A b).det
 
 end algebra
