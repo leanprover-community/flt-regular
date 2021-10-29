bbeedbac17c593fda154c0cfaaf5725c49ec6cac
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-10-29 15:49:42 +0200

    This is false without is_domain

--- src/ready_for_mathlib/cyclotomic.lean
+++ src/ready_for_mathlib/cyclotomic.lean
@@ -10,5 +10,5 @@ begin
     roots_prod_X_sub_C, ← finset.mem_def, ← mem_primitive_roots hpos],
 end
 
-lemma is_root_cyclotomic_iff {n : ℕ} {R : Type*} [comm_ring R] {μ : R}
+lemma is_root_cyclotomic_iff {n : ℕ} {R : Type*} [comm_ring R] [is_domain R] {μ : R}
   : is_primitive_root μ n ↔ is_root (cyclotomic n R) μ := sorry
