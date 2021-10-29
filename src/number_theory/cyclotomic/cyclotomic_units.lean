6768940b16866499663a62c560137da2d29d2561
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-10-29 12:36:14 +0200

    forgot to update this

--- src/number_theory/cyclotomic/cyclotomic_units.lean
+++ src/number_theory/cyclotomic/cyclotomic_units.lean
@@ -21,15 +21,6 @@ variables [comm_ring A] [field K]
 
 section movethis
 
-lemma is_root_cyclotomic_iff {n : ℕ} {K : Type*} [field K] (hpos : 0 < n) {μ : K}
-  (h : ∃ ζ : K, is_primitive_root ζ n) : is_primitive_root μ n ↔ is_root (cyclotomic n K) μ :=
-begin
-  obtain ⟨ζ, hζ⟩ := h,
-  rw [← mem_roots (cyclotomic_ne_zero n K), cyclotomic_eq_prod_X_sub_primitive_roots hζ,
-    roots_prod_X_sub_C, ← finset.mem_def, ← mem_primitive_roots hpos],
-end
--- TODO make a constructor assuming prime, but don't need it here
-
 -- TODO redefine span_singleton as a monoid hom so we get this for free?
 @[simp]
 lemma span_singleton_pow {R : Type*} {P : Type*} [comm_ring R] {S : submonoid R} [comm_ring P]
