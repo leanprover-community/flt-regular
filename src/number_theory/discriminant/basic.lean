4f99adb2229b9ee558e1cc61fd486fd0d69ecf2a
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-10-30 12:57:29 +0200

    add lemmas

--- src/number_theory/discriminant/basic.lean
+++ src/number_theory/discriminant/basic.lean
@@ -3,7 +3,7 @@ import ring_theory.trace
 
 universes u v w
 
-variables (A : Type u) {B : Type v} {ι : Type w} [fintype ι] (b : ι → B)
+variables (A : Type u) {B : Type v} {ι : Type w} (b : ι → B)
 variables [comm_ring A] [comm_ring B] [algebra A B]
 
 open_locale classical
@@ -13,11 +13,33 @@ namespace algebra
 
 /-- Given an `A`-algebra `B` and `b`, an `ι`-indexed family of elements of `B`, we define
 `trace_matrix A ι b` as the matrix whose `(i j)`-th element is the trace of `b i * b j`. -/
-@[nolint unused_arguments]
 def trace_matrix : matrix ι ι A := (λ i j, algebra.trace A B (b i * b j))
 
 /-- Given an `A`-algebra `B` and `b`, an `ι`-indexed family of elements of `B`, we define
 `discriminant A ι b` as the determinant of `trace_matrix A ι b`. -/
-def discriminant := (trace_matrix A b).det
+def discriminant [fintype ι] := (trace_matrix A b).det
+
+lemma discriminant_def [fintype ι] : discriminant A b = (trace_matrix A b).det := rfl
+
+namespace discriminant
+
+variable [fintype ι]
+
+section basic
+
+--discriminant of zero family and similar stuff
+
+end basic
+
+section
+
+lemma zero_of_not_linear_independent (hli : ¬linear_independent A b) : discriminant A b = 0 :=
+sorry
+
+--State the converse with the needed assumptions, fields should be enough for us
+
+end
+
+end discriminant
 
 end algebra
