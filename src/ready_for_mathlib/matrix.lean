5245f0856ed381229257dcb9d8bebf19ee486bb7
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-11-03 15:01:53 +0100

    golf

--- src/ready_for_mathlib/matrix.lean
+++ src/ready_for_mathlib/matrix.lean
@@ -11,11 +11,9 @@ lemma mul_transpose_eq_reindex_mul_reindex_transpose {n : Type u} {m : Type v} {
   M ⬝ Mᵀ = (reindex (equiv.refl n) e M) ⬝ (reindex (equiv.refl n) e M)ᵀ :=
 begin
   ext i j,
-  rw [mul_apply, mul_apply],
-  simp only [id.def, transpose_minor, minor_apply, equiv.refl_symm, reindex_apply, transpose_apply,
-    equiv.coe_refl],
-  refine fintype.sum_equiv e _ _ (λ x, _),
-  simp,
+  simp only [mul_apply, id.def, transpose_minor, minor_apply, equiv.refl_symm, reindex_apply,
+    transpose_apply, equiv.coe_refl],
+  exact fintype.sum_equiv e _ _ (λ x, by simp)
 end
 
 end matrix
