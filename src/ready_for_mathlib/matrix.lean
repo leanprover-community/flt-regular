11836106b48214a67f21d825e70f7ac2a3535eac
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-11-03 15:09:00 +0100

    I don't know why I like golfing so much

--- src/ready_for_mathlib/matrix.lean
+++ src/ready_for_mathlib/matrix.lean
@@ -9,11 +9,6 @@ namespace matrix
 lemma mul_transpose_eq_reindex_mul_reindex_transpose {n : Type u} {m : Type v} {R : Type w}
   [fintype n] [fintype m] [semiring R] (e : m ≃ n) (M : matrix n m R) :
   M ⬝ Mᵀ = (reindex (equiv.refl n) e M) ⬝ (reindex (equiv.refl n) e M)ᵀ :=
-begin
-  ext i j,
-  simp only [mul_apply, id.def, transpose_minor, minor_apply, equiv.refl_symm, reindex_apply,
-    transpose_apply, equiv.coe_refl],
-  exact fintype.sum_equiv e _ _ (λ x, by simp)
-end
+funext $ λ _, funext $ λ _, fintype.sum_equiv e _ _ (λ _, by simp)
 
 end matrix
