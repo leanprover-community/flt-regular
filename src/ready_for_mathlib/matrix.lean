fca709dee1677322dc3ac4ac96d02a6f36601d41
Eric Wieser <wieser.eric@gmail.com>
2021-11-03 16:17:36 +0000

    Switch to a chain of rewrites to show we almost have this result.

--- src/ready_for_mathlib/matrix.lean
+++ src/ready_for_mathlib/matrix.lean
@@ -9,6 +9,7 @@ namespace matrix
 lemma mul_transpose_eq_reindex_mul_reindex_transpose {n : Type u} {m : Type v} {R : Type w}
   [fintype n] [fintype m] [semiring R] (e : m ≃ n) (M : matrix n m R) :
   M ⬝ Mᵀ = (reindex (equiv.refl n) e M) ⬝ (reindex (equiv.refl n) e M)ᵀ :=
-funext $ λ _, funext $ λ _, fintype.sum_equiv e _ _ (λ _, by simp)
+by rw [reindex_apply, transpose_minor, equiv.refl_symm, equiv.coe_refl, ←minor_mul_equiv,
+  minor_id_id]
 
 end matrix
