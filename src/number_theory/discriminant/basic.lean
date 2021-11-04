b61c8e9669f216edfb32ec088c96d9bacc51881e
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-11-04 22:21:02 +0100

    of_power_basis_eq_prod'' proved

--- src/number_theory/discriminant/basic.lean
+++ src/number_theory/discriminant/basic.lean
@@ -5,6 +5,8 @@ import linear_algebra.vandermonde
 
 import ready_for_mathlib.linear_algebra
 import ready_for_mathlib.matrix
+import ready_for_mathlib.fin
+import ready_for_mathlib.nat
 
 universes u v w z
 
@@ -181,10 +183,52 @@ begin
   rw [← prod_pow]
 end
 
+lemma of_power_basis_eq_prod' (e : fin pb.dim ≃ (L →ₐ[K] E)) :
+  algebra_map K E (discriminant K pb.basis) =
+  ∏ i : fin pb.dim, ∏ j in finset.univ.filter (λ j, i < j),
+  -((e j pb.gen- (e i pb.gen)) * (e i pb.gen- (e j pb.gen))) :=
+begin
+  rw [of_power_basis_eq_prod _ _ _ e],
+  congr, ext i, congr, ext j,
+  ring
+end
+
 local notation `n` := finrank K L
 
+lemma of_power_basis_eq_prod'' (e : fin pb.dim ≃ (L →ₐ[K] E)) :
+  algebra_map K E (discriminant K pb.basis) =
+  (-1) ^ (n * (n - 1) / 2) * ∏ i : fin pb.dim, ∏ j in finset.univ.filter (λ j, i < j),
+  ((e j pb.gen- (e i pb.gen)) * (e i pb.gen- (e j pb.gen))) :=
+begin
+  rw [of_power_basis_eq_prod' _ _ _ e],
+  simp_rw [λ i j, neg_eq_neg_one_mul ((e j pb.gen- (e i pb.gen)) * (e i pb.gen- (e j pb.gen))),
+    prod_mul_distrib],
+  congr,
+  simp only [prod_pow_eq_pow_sum, prod_const],
+  congr,
+  simp_rw [fin.filter_gt_card],
+  apply (@nat.cast_inj ℚ _ _ _ _ _).1,
+  rw [nat.cast_sum],
+  have : ∀ (x : fin pb.dim), (↑x + 1) ≤ pb.dim := by simp [nat.succ_le_iff, fin.is_lt],
+  simp_rw [nat.sub_sub],
+  simp only [nat.cast_sub, this, finset.card_fin, nsmul_eq_mul, sum_const, sum_sub_distrib,
+    nat.cast_add, nat.cast_one, sum_add_distrib, mul_one],
+  rw [← nat.cast_sum, ← @finset.sum_range ℕ _ pb.dim (λ i, i), sum_range_id ],
+  have hn : n = pb.dim,
+  { rw [← alg_hom.card K L E, ← fintype.card_fin pb.dim],
+    exact card_congr (equiv.symm e) },
+  have h₂ : 2 ∣ (pb.dim * (pb.dim - 1)) := even_iff_two_dvd.1 (nat.even_mul_self_pred _),
+  have hne : ((2 : ℕ) : ℚ) ≠ 0 := by simp,
+  have hle : 1 ≤ pb.dim,
+  { rw [← hn, nat.one_le_iff_ne_zero, ← zero_lt_iff, finite_dimensional.finrank_pos_iff],
+    apply_instance },
+  rw [hn, nat.cast_dvd h₂ hne, nat.cast_mul, nat.cast_sub hle],
+  field_simp,
+  ring,
+end
+
 lemma of_power_basis_eq_norm : discriminant K pb.basis =
-  (-1) ^ (n * (n - 1) / 2) *(norm K (aeval pb.gen (minpoly K pb.gen).derivative)) := sorry
+  (-1) ^ (n * (n - 1) / 2) * (norm K (aeval pb.gen (minpoly K pb.gen).derivative)) := sorry
 
 end field
 
