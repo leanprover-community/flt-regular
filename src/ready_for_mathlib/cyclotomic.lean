742ed557d2aa16e764f69bdb9b5fd2e9652183be
Eric <ericrboidi@gmail.com>
2021-11-16 16:27:36 +0000

    tidy for mathlib

--- src/ready_for_mathlib/cyclotomic.lean
+++ src/ready_for_mathlib/cyclotomic.lean
@@ -1,4 +1,5 @@
 import ring_theory.polynomial.cyclotomic
+import ready_for_mathlib.roots_of_unity
 
 open polynomial nat
 open_locale big_operators
@@ -15,84 +16,12 @@ lemma is_root_of_unity_iff {n : ℕ} (h : 0 < n) (R : Type*) [comm_ring R] [is_d
 by rw [←mem_nth_roots h, nth_roots, mem_roots $ X_pow_sub_C_ne_zero h _,
        C_1, ←prod_cyclotomic_eq_X_pow_sub_one h, is_root_prod]; apply_instance
 
-@[nontriviality] lemma is_primitive_root.of_subsingleton {M : Type*}
-  [comm_monoid M] [subsingleton M] (x : M) : is_primitive_root x 1 :=
-⟨subsingleton.elim _ _, λ _ _, one_dvd _⟩
-
-lemma is_primitive_root.zero {R : Type*} [comm_semiring R] [nontrivial R] :
-  is_primitive_root (0 : R) 0 :=
--- why does simpa need so much hand-holding...
-⟨pow_zero 0, λ l hl, by simpa [zero_pow_eq, show ∀ p, ¬p → false ↔ p, by tauto!] using hl⟩
-
-lemma is_primitive_root.order_of {M : Type*} [comm_monoid M] (ζ : M) :
-  is_primitive_root ζ (order_of ζ) :=
-⟨pow_order_of_eq_one ζ, λ l, order_of_dvd_of_pow_eq_one⟩
-
-lemma is_primitive_root.unique {n m : ℕ} {M : Type*} [comm_monoid M] {ζ : M}
-  (hn : is_primitive_root ζ n) (hm : is_primitive_root ζ m) : n = m :=
-begin
-  wlog hnm : n ≤ m,
-  rcases hnm.eq_or_lt with rfl | hnm,
-  { refl },
-  rcases n.eq_zero_or_pos with rfl | hn',
-  { exact (zero_dvd_iff.mp $ hn.dvd_of_pow_eq_one m hm.pow_eq_one).symm },
-  exact absurd hn.pow_eq_one (hm.pow_ne_one_of_pos_of_lt hn' hnm)
-end
-
-lemma is_primitive_root.eq_order_of {n : ℕ} {M : Type*} [comm_monoid M] {ζ : M}
-  (h : is_primitive_root ζ n) : n = order_of ζ :=
-h.unique (is_primitive_root.order_of ζ)
-
--- equivalent statement for n : ℕ availab,e but too lazy
-lemma is_primitive_root_iff {n : ℕ+} {M : Type*} [comm_monoid M] (ζ : M) :
-  is_primitive_root ζ n ↔ ζ ^ (n : ℕ) = 1 ∧ ∀ l : ℕ, 0 < l → l < n → ζ ^ l ≠ 1 :=
-begin
-  refine ⟨λ h, ⟨h.pow_eq_one, λ l hl' hl, _⟩, λ ⟨hζ, hl⟩, is_primitive_root.mk_of_lt ζ n.pos hζ hl⟩,
-  rw h.eq_order_of at hl,
-  exact pow_ne_one_of_lt_order_of' hl'.ne' hl,
-end
-
-lemma is_not_primitive_root_iff {n : ℕ} {M : Type*} [comm_monoid M] (ζ : M) :
-  ¬ is_primitive_root ζ n ↔ order_of ζ ≠ n :=
-⟨λ h hn, h $ hn ▸ is_primitive_root.order_of ζ,
- λ h hn, h.symm $ hn.unique $ is_primitive_root.order_of ζ⟩
-
-lemma is_primitive_root.of_injective {R S : Type*} [comm_semiring R] [comm_semiring S] {f : R →+* S}
-  (hf : function.injective f) {x : R} {n : ℕ} (hx : is_primitive_root x n) :
-  is_primitive_root (f x) n :=
-{ pow_eq_one := by rw [←f.map_pow, hx.pow_eq_one, f.map_one],
-  dvd_of_pow_eq_one := begin
-    have := hx.eq_order_of,
-    subst this,
-    intros l hl,
-    rw [←f.map_pow, ←f.map_one] at hl,
-    exact order_of_dvd_of_pow_eq_one (hf hl)
-  end }
-
-lemma is_primitive_root.of_injective' {R S : Type*} [comm_semiring R] [comm_semiring S]
-  {f : R →+* S} (hf : function.injective f) {x : R} {n : ℕ} (hx : is_primitive_root (f x) n) :
-  is_primitive_root x n :=
-{ pow_eq_one := by { apply_fun f, rw [f.map_pow, f.map_one, hx.pow_eq_one] },
-  dvd_of_pow_eq_one := begin
-    have := hx.eq_order_of,
-    subst this,
-    intros l hl,
-    apply_fun f at hl,
-    rw [f.map_pow, f.map_one] at hl,
-    exact order_of_dvd_of_pow_eq_one hl
-  end }
-
-lemma is_primitive_root.injective_iff {R S : Type*} [comm_semiring R] [comm_semiring S]
-  {f : R →+* S} (hf : function.injective f) {x : R} {n : ℕ} :
-  is_primitive_root x n ↔ is_primitive_root (f x) n :=
-⟨λ h, h.of_injective hf, λ h, h.of_injective' hf⟩
-
 open finset
 
 lemma nat.mem_divisors_self (n : ℕ) (h : n ≠ 0) : n ∈ n.divisors := by simpa
 
--- this needs to replace `polynomial.order_of_root_cyclotomic_eq` when it is merged
-lemma is_root_cyclotomic_iff' {n : ℕ} {K : Type*} [field K] {μ : K} (hn : (n : K) ≠ 0) :
+-- `is_root_cyclotomic_iff` is strictly stronger, this is a necessary intermediate result
+private lemma is_root_cyclotomic_iff' {n : ℕ} {K : Type*} [field K] {μ : K} (hn : (n : K) ≠ 0) :
   is_primitive_root μ n ↔ is_root (cyclotomic n K) μ :=
 begin
   -- in this proof, `o` stands for `order_of μ`
@@ -132,14 +61,14 @@ lemma is_root_map {R S : Type*} [comm_ring R] [comm_semiring S] {f : R →+* S} 
   {p : polynomial R} (hf : function.injective f) : is_root p x ↔ is_root (p.map f) (f x) :=
 by simp only [is_root, eval_map, eval₂_hom, f.injective_iff'.mp hf]
 
+-- this needs to replace `polynomial.order_of_root_cyclotomic_eq` when it is merged
 lemma is_root_cyclotomic_iff {n : ℕ} {R : Type*} [comm_ring R] [is_domain R]
   {μ : R} (hn : (n : R) ≠ 0) : is_primitive_root μ n ↔ is_root (cyclotomic n R) μ :=
 begin
   let f := algebra_map R (fraction_ring R),
   have hf : function.injective f := is_localization.injective _ le_rfl,
-  rw [is_root_map hf, is_primitive_root.injective_iff hf, map_cyclotomic,
-    ← is_root_cyclotomic_iff' (λ h, by simpa [ring_hom.map_nat_cast, hn, h] using
-    ((ring_hom.injective_iff f).1 (is_fraction_ring.injective R _) n))],
+  rw [is_root_map hf, is_primitive_root.injective_iff hf, map_cyclotomic, ←is_root_cyclotomic_iff'],
+  simpa only [f.map_nat_cast, hn] using f.injective_iff.mp hf n
 end
 
 namespace polynomial
