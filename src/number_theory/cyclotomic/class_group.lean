9527a2f92bcb0f85ac5e9863d461a233b8ed0589
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-11-17 13:57:26 +0100

    fix build

--- src/number_theory/cyclotomic/class_group.lean
+++ src/number_theory/cyclotomic/class_group.lean
@@ -8,6 +8,8 @@ variable (n : ℕ+)
 
 section finiteness
 
+local attribute [instance] is_cyclotomic_extension.finite_dimensional
+
 noncomputable instance class_group.fintype_of_cyclotomic_ring (n : ℕ+) :
   fintype (class_group (cyclotomic_ring n ℤ ℚ) (cyclotomic_field n ℚ)) :=
 class_group.fintype_of_admissible_of_finite ℚ (cyclotomic_field n ℚ)
