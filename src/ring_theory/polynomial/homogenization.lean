2a0988152653e5f99192940a2ae3e495f1522c9b
Alex J. Best <alex.j.best@gmail.com>
2021-11-21 00:48:37 +0100

    bunch of material on leading terms

--- src/ring_theory/polynomial/homogenization.lean
+++ src/ring_theory/polynomial/homogenization.lean
@@ -175,6 +175,14 @@ begin
     tsub_self, ← hs, finsupp.update_self],
 end
 
+lemma is_homogeneous_homogenization (i : ι) (p : mv_polynomial ι R)
+  (h : ∀ j ∈ p.support, (j : ι → ℕ) i = 0) :
+  (p.homogenization i).is_homogeneous p.total_degree :=
+begin
+  sorry
+end
+
+-- TODO this can follow from previous
 lemma total_degree_homogenization (i : ι) (p : mv_polynomial ι R)
   (h : ∀ j ∈ p.support, (j : ι → ℕ) i = 0) :
   (p.homogenization i).total_degree = p.total_degree :=
@@ -195,19 +203,142 @@ begin
   { simp, sorry, },
 end
 
-lemma is_homogenous_homogenization (i : ι) (p : mv_polynomial ι R)
-  (h : ∀ j ∈ p.support, (j : ι → ℕ) i = 0) :
-  (p.homogenization i).is_homogeneous p.total_degree :=
+section leading_terms
+-- TODO is this the best def?
+/-- The sum of the monomials of highest degree of a multivariate polynomial. -/
+def leading_terms (p : mv_polynomial ι R) : mv_polynomial ι R :=
+(p.support.filter (λ s : ι →₀ ℕ, s.sum (λ _ e, e) = p.total_degree)).sum $
+  λ s, monomial s (p.coeff s)
+
+@[simp]
+lemma leading_terms_zero : (0 : mv_polynomial ι R).leading_terms = 0 :=
+by simp [leading_terms]
+
+lemma is_homogeneous_leading_terms (p : mv_polynomial ι R) :
+  p.leading_terms.is_homogeneous p.total_degree :=
 begin
-  sorry
+  rw leading_terms,
+  rw is_homogeneous,
+  intros d hd,
+  simp at hd,
+  sorry,
 end
 
+@[simp] lemma support_zero : (0 : mv_polynomial ι R).support = ∅ := finsupp.support_zero
+
+lemma support_add_eq [decidable_eq ι] {g₁ g₂ : mv_polynomial ι R}
+  (h : disjoint g₁.support g₂.support) : (g₁ + g₂).support = g₁.support ∪ g₂.support :=
+finsupp.support_add_eq h
+
+lemma add_ne_zero_of_ne_zero_of_support_disjoint [decidable_eq ι] (p q : mv_polynomial ι R)
+  (hp : p ≠ 0) (h : disjoint p.support q.support) : p + q ≠ 0 :=
+begin
+  contrapose! hp,
+  have := congr_arg support hp,
+  rw [support_zero, support_add_eq h, finset.union_eq_empty_iff, -- TODO should this be simp?
+    mv_polynomial.support_eq_empty] at this,
+  exact this.left,
+end
+
+@[simp] lemma monomial_eq_zero (a : ι →₀ ℕ) (b : R) : monomial a b = 0 ↔ b = 0 :=
+finsupp.single_eq_zero
+
+lemma support_sum_monomial_subset [decidable_eq ι] (S : finset (ι →₀ ℕ)) (f : (ι →₀ ℕ) → R) :
+  support (∑ v in S, monomial v (f v)) ⊆ S :=
+begin
+  induction S using finset.induction with s S hs hsi,
+  { simp, },
+  { rw finset.sum_insert hs,
+    apply finset.subset.trans support_add,
+    apply finset.union_subset,
+    { apply finset.subset.trans support_monomial_subset (finset.subset_union_left _ S), },
+    { apply finset.subset.trans hsi (finset.subset_insert _ _), }, },
+end
+
+-- TODO probably other versions of this lemma for mathlib
+lemma ite_subset_union {α : Type*} [decidable_eq α] (s s' : finset α) (P : Prop) [decidable P] :
+  ite P s s' ⊆ s ∪ s' :=
+begin
+  split_ifs,
+  exact finset.subset_union_left s s',
+  exact finset.subset_union_right s s',
+end
+
+-- TODO probably there is a better proof?
+lemma sum_monomial_ne_zero_of_exists_mem_ne_zero [decidable_eq ι] [decidable_eq R]
+  (S : finset (ι →₀ ℕ)) (f : (ι →₀ ℕ) → R) (h : ∃ (s) (hs : s ∈ S), f s ≠ 0) :
+  ∑ (s : ι →₀ ℕ) in S, monomial s (f s) ≠ 0 :=
+begin
+  induction S using finset.induction with s S hs hsi,
+  { simpa using h, },
+  rw finset.sum_insert hs,
+  simp only [exists_prop, finset.mem_insert] at h,
+  rcases h with ⟨h, rfl | h_h_left, h_h_right⟩,
+  { apply add_ne_zero_of_ne_zero_of_support_disjoint,
+    { simpa, },
+    { simp only [support_monomial, h_h_right, not_not, mem_support_iff, if_false,
+        finset.disjoint_singleton_left],
+      rw coeff_sum,
+      simp_rw [coeff_monomial],
+      conv in (ite _ _ _)
+      { rw if_neg (ne_of_mem_of_not_mem H hs : ¬ x = h), }, -- using classical makes this line fail
+      simp, }, },
+  { rw add_comm,
+    apply add_ne_zero_of_ne_zero_of_support_disjoint,
+    { apply hsi ⟨h, h_h_left, h_h_right⟩, },
+    { simp only [support_monomial],
+      apply finset.disjoint_of_subset_right,
+      exact ite_subset_union _ _ _,
+      simp only [finset.disjoint_singleton_right, finset.empty_union, not_not, mem_support_iff],
+      rw coeff_sum,
+      simp_rw [coeff_monomial],
+      conv in (ite _ _ _)
+      { rw if_neg (ne_of_mem_of_not_mem H hs : ¬ x = s), }, -- using classical makes this line fail
+      simp, }, },
+end
+
+lemma exists_coeff_ne_zero_total_degree {p : mv_polynomial ι R} (hp : p ≠ 0) :
+  ∃ (v : ι →₀ ℕ), v.sum (λ _ e, e) = p.total_degree ∧ p.coeff v ≠ 0 :=
+begin
+  obtain ⟨b, hb₁, hb₂⟩ := p.support.exists_mem_eq_sup (finsupp.support_nonempty_iff.mpr hp)
+    (λ (m : ι →₀ ℕ), m.to_multiset.card),
+  use b,
+  split,
+  { rw ← total_degree_eq p at hb₂,
+    rw hb₂,
+    dsimp, -- TODO break this out as a lemma
+    funext m,
+    exact (finsupp.card_to_multiset _).symm, },
+  { exact mem_support_iff.mp hb₁, },
+end
+
+lemma leading_terms_ne_zero {p : mv_polynomial ι R} (hp : p ≠ 0) : p.leading_terms ≠ 0 :=
+begin
+  classical,
+  rw leading_terms,
+  apply sum_monomial_ne_zero_of_exists_mem_ne_zero,
+  simp only [exists_prop, mem_support_iff, finset.mem_filter],
+  convert exists_coeff_ne_zero_total_degree hp,
+  ext v,
+  simp [and_comm],
+end
+
+lemma total_degree_leading_terms (p : mv_polynomial ι R) :
+  p.leading_terms.total_degree = p.total_degree :=
+begin
+  by_cases hp : p = 0,
+  { simp [hp], },
+  rw is_homogeneous.total_degree (is_homogeneous_leading_terms p) (leading_terms_ne_zero hp),
+end
+
+end leading_terms
 
 lemma homogenization_mul (i : ι) (p q : mv_polynomial ι R)
   (hp : ∀ j ∈ p.support, (j : ι → ℕ) i = 0) (hq : ∀ j ∈ q.support, (j : ι → ℕ) i = 0) :
   (p * q).homogenization i = p.homogenization i * q.homogenization i :=
 begin
-  -- have := total_degree_mul,
+  simp [homogenization],
+  -- rw finsupp.map_domain_mul,
   sorry,
 end
 
