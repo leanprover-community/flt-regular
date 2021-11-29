98308939914bc04b667877f3b019b20fa4ee8a66
Alex J. Best <alex.j.best@gmail.com>
2021-11-29 19:10:32 +0100

    add version

--- src/number_theory/cyclotomic/factoring.lean
+++ src/number_theory/cyclotomic/factoring.lean
@@ -37,7 +37,7 @@ end mv_polynomial
 
 /-- If there is a primitive `n`th root of unity in `K`, then `X ^ n - Y ^ n = ∏ (X - μ Y)`,
 where `μ` varies over the `n`-th roots of unity. -/
-lemma pow_add_pow_eq_prod_add_zeta_mul {K : Type*} [field K] {ζ : K} {n : ℕ} (hpos : 0 < n)
+lemma pow_sub_pow_eq_prod_sub_zeta_mul {K : Type*} [field K] {ζ : K} {n : ℕ} (hpos : 0 < n)
   (h : is_primitive_root ζ n) (x y : K) :
   x ^ (n : ℕ) - y ^ (n : ℕ) = ∏ (ζ : K) in nth_roots_finset n K, (x - ζ * y) :=
 begin
@@ -53,4 +53,17 @@ begin
   rw [homogenization_prod, algebra_map_eq, ← mv_polynomial.C_1] at hh, -- TODO homog x_pow_sub_one?
   simp only [hpos, homogenization_X_pow_sub_C, homogenization_X_sub_C] at hh,
   simpa using hh,
+end
+
+/-- If there is a primitive `n`th root of unity in `K` and `n` is odd, then
+`X ^ n + Y ^ n = ∏ (X + μ Y)`, where `μ` varies over the `n`-th roots of unity. -/
+lemma pow_add_pow_eq_prod_add_zeta_mul {K : Type*} [field K] {ζ : K} {n : ℕ} (hodd : n % 2 = 1)
+  (h : is_primitive_root ζ n) (x y : K) :
+  x ^ (n : ℕ) + y ^ (n : ℕ) = ∏ (ζ : K) in nth_roots_finset n K, (x + ζ * y) :=
+begin
+  -- TODO rename nat.odd_gt_zero
+  have := pow_sub_pow_eq_prod_sub_zeta_mul (nat.odd_gt_zero (nat.odd_iff.mpr hodd)) h x (-y),
+  simp only [mul_neg_eq_neg_mul_symm, sub_neg_eq_add] at this,
+  rw [neg_pow, neg_one_pow_eq_pow_mod_two] at this,
+  simpa [hodd] using this,
 end
