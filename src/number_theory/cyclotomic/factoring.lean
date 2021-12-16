7182b093689b98925ef8029a3f0683b14ce2d8a0
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-12-16 20:35:24 +0100

    fix build

--- src/number_theory/cyclotomic/factoring.lean
+++ src/number_theory/cyclotomic/factoring.lean
@@ -3,7 +3,7 @@ Copyright (c) 2021 Alex J. Best. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Alex J. Best
 -/
-import number_theory.cyclotomic.basic
+import ready_for_mathlib.cyclotomic.basic
 import ring_theory.polynomial.homogenization
 
 open_locale big_operators
