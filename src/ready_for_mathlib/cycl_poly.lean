a170c0b191403a24ccd302911893230280927a28
Riccardo Brasca <riccardo.brasca@gmail.com>
2021-12-23 21:00:53 +0100

    add lemmas

--- src/ready_for_mathlib/cycl_poly.lean
+++ src/ready_for_mathlib/cycl_poly.lean
@@ -126,3 +126,46 @@ begin
       nat.totient_prime hp, mul_comm (p - 1), ← nat.mul_succ, nat.sub_one,
       nat.succ_pred_eq_of_pos hp.pos] }
 end
+
+lemma cyclotomic_mul_prime_not_dvd_eq_pow (R : Type*) {p n : ℕ} [hp : fact (nat.prime p)] [ring R]
+  [char_p R p] (hn : ¬p ∣ n) : cyclotomic (n * p) R = (cyclotomic n R) ^ (p - 1) :=
+begin
+  suffices : cyclotomic (n * p) (zmod p) = (cyclotomic n (zmod p)) ^ (p - 1),
+  { rw [← map_cyclotomic _ (algebra_map (zmod p) R), ← map_cyclotomic _ (algebra_map (zmod p) R),
+      this, polynomial.map_pow] },
+  suffices : cyclotomic (n * p) (zmod p) * (cyclotomic n (zmod p)) = (cyclotomic n (zmod p)) ^ p,
+  { have h : cyclotomic n (zmod p) ^ p = cyclotomic n (zmod p) ^ p.pred.succ,
+    { rw [nat.succ_pred_eq_of_pos hp.out.pos] },
+    rw [h, pow_succ, mul_comm (cyclotomic (n * p) _)] at this,
+    exact mul_right_injective₀ (cyclotomic_ne_zero _ _) this },
+  rw [← zmod.expand_card],
+  nth_rewrite 2 [← map_cyclotomic_int],
+  rw [← map_expand, cyclotomic_expand_eq_cyclotomic_mul hp.out hn, polynomial.map_mul,
+    map_cyclotomic, map_cyclotomic]
+end
+
+lemma cyclotomic_mul_prime_dvd_eq_pow (R : Type*) {p n : ℕ} [hp : fact (nat.prime p)] [ring R]
+  [char_p R p] (hn : p ∣ n) : cyclotomic (n * p) R = (cyclotomic n R) ^ p :=
+begin
+  suffices : cyclotomic (n * p) (zmod p) = (cyclotomic n (zmod p)) ^ p,
+  { rw [← map_cyclotomic _ (algebra_map (zmod p) R), ← map_cyclotomic _ (algebra_map (zmod p) R),
+      this, polynomial.map_pow] },
+  rw [← zmod.expand_card, ← map_cyclotomic_int n, ← map_expand, cyclotomic_expand_eq_cyclotomic
+    hp.out hn, map_cyclotomic, mul_comm]
+end
+
+lemma cyclotomic_mul_prime_pow_eq (R : Type*) {p m k : ℕ} [hp : fact (nat.prime p)] [ring R]
+  [char_p R p] (hk : 0 < k) (hm : ¬p ∣ m) :
+  cyclotomic (p ^ k * m) R = (cyclotomic m R) ^ (p ^ k - p ^ (k - 1)) :=
+begin
+  obtain ⟨a, ha⟩ := nat.exists_eq_succ_of_ne_zero hk.ne',
+  rw [ha],
+  clear ha hk,
+  induction a with a H,
+  { rw [pow_one, nat.sub_self, pow_zero, mul_comm, cyclotomic_mul_prime_not_dvd_eq_pow R hm] },
+  { have hdiv : p ∣ p ^ a.succ * m := ⟨p ^ a * m, by rw [← mul_assoc, pow_succ]⟩,
+    rw [pow_succ, mul_assoc, mul_comm, cyclotomic_mul_prime_dvd_eq_pow R hdiv, H, ← pow_mul],
+    congr' 1,
+    simp only [tsub_zero, nat.succ_sub_succ_eq_sub],
+    rw [nat.mul_sub_right_distrib, mul_comm, pow_succ']  }
+end
