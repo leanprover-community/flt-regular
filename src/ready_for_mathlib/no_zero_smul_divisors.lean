00f0213340a8bedb2d34d0a44f86ae6d7e8b5f51
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-01-04 00:06:51 +0100

    lint

--- src/ready_for_mathlib/no_zero_smul_divisors.lean
+++ src/ready_for_mathlib/no_zero_smul_divisors.lean
@@ -2,7 +2,7 @@ import ring_theory.localization
 
 namespace is_fraction_ring
 
-instance {R K} [comm_ring R] [field K] [algebra R K] [is_fraction_ring R K] :
+@[priority 100] instance {R K} [comm_ring R] [field K] [algebra R K] [is_fraction_ring R K] :
   no_zero_smul_divisors R K :=
 no_zero_smul_divisors.of_algebra_map_injective $ is_fraction_ring.injective R K
 
