4d5d3ac4d890e046a71b24c159c249ae1d2eb388
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-01-10 15:49:55 +0100

    lint

--- src/ready_for_mathlib/discriminant.lean
+++ src/ready_for_mathlib/discriminant.lean
@@ -5,12 +5,13 @@ namespace algebra
 
 open matrix
 
-variables {R K L ι : Type*} [comm_ring R] [field K] [field L] [fintype ι] [decidable_eq ι]
-variables [algebra R K] [algebra K L] [algebra R L][is_scalar_tower R K L] [finite_dimensional K L]
+variables {R K L ι : Type*} [comm_ring R] [field K] [field L] [fintype ι]
+variables [algebra R K] [algebra K L] [algebra R L][is_scalar_tower R K L]
 
-lemma discr_is_integral {b : ι → L} (h : ∀ i, _root_.is_integral R (b i)) :
+lemma discr_is_integral [finite_dimensional K L] {b : ι → L} (h : ∀ i, _root_.is_integral R (b i)) :
   _root_.is_integral R (discr K b) :=
 begin
+  classical,
   rw [discr_def],
   exact det_is_integral (λ i j, is_integral_trace (is_integral_mul (h i) (h j)))
 end
