5eda4025489a94d46b01e357c8161d9174263c83
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-01-10 21:52:05 +0100

    ops

--- src/ready_for_mathlib/intermediate_field.lean
+++ src/ready_for_mathlib/intermediate_field.lean
@@ -6,6 +6,20 @@ open_locale big_operators
 
 variables {K L ι : Type*} [field K] [field L] [algebra K L] {S : intermediate_field K L}
 
-lemma coe_sum [fintype ι] (f : ι → S) : (∑ i, f i : L) = ∑ i, (f i : L) := rfl
+lemma coe_sum [fintype ι] (f : ι → S) : (↑∑ i, f i : L) = ∑ i, (f i : L) :=
+begin
+  classical,
+  induction finset.univ using finset.induction_on with i s hi H,
+  { simp },
+  { rw [finset.sum_insert hi, coe_add, H, finset.sum_insert hi] }
+end
+
+lemma coe_prod [fintype ι] (f : ι → S) : (↑∏ i, f i : L) = ∏ i, (f i : L) :=
+begin
+  classical,
+  induction finset.univ using finset.induction_on with i s hi H,
+  { simp },
+  { rw [finset.prod_insert hi, coe_mul, H, finset.prod_insert hi] }
+end
 
 end intermediate_field
