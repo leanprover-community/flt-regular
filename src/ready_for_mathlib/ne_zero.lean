3f0ecfbd6909699fad7d8cc6f57acfa09b2a55af
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-01-15 10:53:27 +0100

    Delete ne_zero.lean

--- src/ready_for_mathlib/ne_zero.lean
+++
@@ -1,6 +0,0 @@
-import algebra.ne_zero
-
--- pr #11437
-lemma pos_of_ne_zero_coe (n : ℕ) (R : Type*) [has_zero R] [has_one R] [has_add R]
-  [ne_zero (n : R)] : 0 < n :=
-(ne_zero.of_ne_zero_coe R).out.bot_lt
