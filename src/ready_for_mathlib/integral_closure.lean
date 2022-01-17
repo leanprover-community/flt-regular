28dc87f3b330a4f8224d78b7c37d73fb810fd6d6
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-01-17 11:40:59 +0100

    better name

--- src/ready_for_mathlib/integral_closure.lean
+++ src/ready_for_mathlib/integral_closure.lean
@@ -4,7 +4,7 @@ universes u v
 
 variables {R : Type u} {S : Type v} [comm_ring R] [comm_ring S] [algebra R S]
 
-lemma is_integral.smul {x : S} (r : R) (hx : is_integral R x) : is_integral R (r • x) :=
+lemma is_integral_smul {x : S} (r : R) (hx : is_integral R x) : is_integral R (r • x) :=
 begin
   rw [algebra.smul_def],
   refine is_integral_mul is_integral_algebra_map hx,
