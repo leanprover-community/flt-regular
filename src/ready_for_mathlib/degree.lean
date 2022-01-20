797c5ea4a26f2cf909f664eaf0e408be0b1abcc8
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-01-20 15:40:38 +0100

    better statement and simp

--- src/ready_for_mathlib/degree.lean
+++ src/ready_for_mathlib/degree.lean
@@ -6,20 +6,22 @@ namespace polynomial
 
 variables {R : Type u} {S : Type v} [semiring R] [semiring S]
 
+@[simp]
 lemma nat_degree_map_of_monic [nontrivial S] {P : polynomial R} (hmo : P.monic) (f : R →+* S) :
-  P.nat_degree = (P.map f).nat_degree :=
+  (P.map f).nat_degree = P.nat_degree :=
 begin
-  refine le_antisymm (le_nat_degree_of_ne_zero _) (nat_degree_map_le _ _),
+  refine le_antisymm (nat_degree_map_le _ _) (le_nat_degree_of_ne_zero _),
   rw [coeff_map, monic.coeff_nat_degree hmo, ring_hom.map_one],
   exact one_ne_zero
 end
 
+@[simp]
 lemma degree_map_of_monic [nontrivial S] {P : polynomial R} (hmo : P.monic) (f : R →+* S) :
-  P.degree = (P.map f).degree :=
+  (P.map f).degree = P.degree :=
 begin
   by_cases hP : P = 0,
   { simp [hP] },
-  { refine le_antisymm _ (degree_map_le _ _),
+  { refine le_antisymm (degree_map_le _ _) _,
     rw [degree_eq_nat_degree hP],
     refine le_degree_of_ne_zero _,
     rw [coeff_map, monic.coeff_nat_degree hmo, ring_hom.map_one],
