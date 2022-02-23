e6bf8012e3ebcd9f3bb23d5a5af8149b3f6523db
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-02-23 18:09:18 +0100

    kill two sorrys

--- src/ready_for_mathlib/discriminant/power_basis.lean
+++ src/ready_for_mathlib/discriminant/power_basis.lean
@@ -1,25 +1,46 @@
 import ring_theory.power_basis
 import linear_algebra.matrix.basis
+import ring_theory.algebraic
+import ring_theory.adjoin.power_basis
 
 universes u v z
 
-variables {R : Type u} {S : Type v} [comm_ring R] [comm_ring S] [algebra R S] (B : power_basis R S)
+variables {R : Type z} {S : Type v} [comm_ring R] [comm_ring S] [algebra R S]
+variables {K : Type u} {L : Type v} [field K] [field L] [algebra K L] (B : power_basis K L)
 
-open algebra polynomial
+open algebra polynomial subalgebra algebra.adjoin
 
 namespace power_basis
 
-/-- The power basis given by `s` if `B.gen ∈ adjoin R {s}`. -/
-def of_mem_adjon {s : S} (hs : B.gen ∈ adjoin R ({s} : set S)) :
-  power_basis R S := sorry
+lemma adjoin_gen_eq_top (B : power_basis R S) : adjoin R ({B.gen} : set S) = ⊤ :=
+begin
+  rw [← to_submodule_eq_top, _root_.eq_top_iff, ← B.basis.span_eq, submodule.span_le],
+  rintros x ⟨i, rfl⟩,
+  rw [B.basis_eq_pow i],
+  exact subalgebra.pow_mem _ (subset_adjoin (set.mem_singleton _)) _,
+end
 
-lemma of_mem_adjon_gen {s : S} (hs : B.gen ∈ adjoin R ({s} : set S)) :
-  (B.of_mem_adjon hs).gen = s := sorry
+lemma adjoin_eq_top_of_gen_mem (B : power_basis R S) {x : S} (hx : B.gen ∈ adjoin R ({x} : set S)) :
+  adjoin R ({x} : set S) = ⊤ :=
+begin
+  rw [_root_.eq_top_iff, ← adjoin_gen_eq_top B],
+  refine adjoin_le _,
+  simp [hx],
+end
 
-variables {A : Type z} [comm_ring A] [algebra A R] [algebra A S] {B}
+/-- The power basis given by `x` if `B.gen ∈ adjoin K {x}`. -/
+noncomputable def of_mem_adjon {x : L} (hx : B.gen ∈ adjoin K ({x} : set L)) :
+  power_basis K L :=
+by { letI := B.finite_dimensional, exact (power_basis $ is_integral_of_finite K L x).map
+  ((equiv_of_eq _ _ $ adjoin_eq_top_of_gen_mem B hx).trans top_equiv) }
 
-lemma to_matrix_is_integral_coeff {B' : power_basis R S}
-  (h : ∃ (P : polynomial A), aeval B.gen P = B'.gen) :
-  ∀ i j, _root_.is_integral A (B.basis.to_matrix B'.basis i j) := sorry
+lemma of_mem_adjon_gen {x : L} (hs : B.gen ∈ adjoin K ({x} : set L)) :
+  (B.of_mem_adjon hs).gen = x := rfl
+
+variables [algebra R K] [algebra R L] {B}
+
+lemma to_matrix_is_integral_coeff {B' : power_basis K L}
+  (h : ∃ (P : polynomial R), aeval B.gen P = B'.gen) :
+  ∀ i j, _root_.is_integral R (B.basis.to_matrix B'.basis i j) := sorry
 
 end power_basis
