97ac506c6bcea719dde6d0990e6ba1b54396c09a
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-02-24 21:30:22 +0100

    to_matrix_is_integral_coeff proved

--- src/ready_for_mathlib/discriminant/power_basis.lean
+++ src/ready_for_mathlib/discriminant/power_basis.lean
@@ -17,9 +17,9 @@ open_locale polynomial
 
 namespace power_basis
 
-lemma is_integral_coord_gen_pow  {A : Type w} [comm_ring A] [algebra R A] [algebra S A]
-  [is_scalar_tower R S A] [is_domain S] {B : power_basis S A} (hB : is_integral R B.gen) (n : ℕ)
-  (hmin : minpoly S B.gen = (minpoly R B.gen).map (algebra_map R S)) :
+lemma is_integral_coord_gen_pow {A : Type w} [comm_ring A] [algebra R A] [algebra S A]
+  [is_scalar_tower R S A] [is_domain S] {B : power_basis S A} (hB : is_integral R B.gen)
+  (hmin : minpoly S B.gen = (minpoly R B.gen).map (algebra_map R S)) (n : ℕ) :
   ∀ i, is_integral R (B.basis.repr (B.gen ^ n) i) :=
 begin
   intro i,
@@ -39,7 +39,7 @@ begin
   simp only [linear_equiv.map_sum, linear_equiv.map_smulₛₗ, ring_hom.id_apply, finset.sum_apply'],
   refine is_integral.sum _ (λ j hj, _),
   replace hj := finset.mem_range.1 hj,
-  rw [← fin.coe_mk hj, ← B.basis_eq_pow ⟨j, hj⟩, algebra.smul_def,
+  rw [← fin.coe_mk hj, ← B.basis_eq_pow, algebra.smul_def,
     is_scalar_tower.algebra_map_apply R S A, ← algebra.smul_def, linear_equiv.map_smul],
   simp only [algebra_map_smul, finsupp.coe_smul, pi.smul_apply, B.basis.repr_self_apply],
   by_cases hij : (⟨j, hj⟩ : fin _) = i,
@@ -63,7 +63,31 @@ begin
     ring_hom.id_apply, finsupp.coe_smul, pi.smul_apply, id.smul_eq_mul],
   refine is_integral_mul (hy _) (is_integral_mul (hx _) _),
   simp only [coe_basis, ← pow_add],
-  refine is_integral_coord_gen_pow hB _ hmin _,
+  refine is_integral_coord_gen_pow hB hmin _ _,
+end
+
+lemma is_integral_coord_pow {A : Type w} [comm_ring A] [algebra R A] [algebra S A] [is_domain S]
+  [is_scalar_tower R S A] {B : power_basis S A} {x : A} (hB : is_integral R B.gen)
+  (hx : ∀ i, is_integral R (B.basis.repr x i))
+  (hmin : minpoly S B.gen = (minpoly R B.gen).map (algebra_map R S)) (n : ℕ) :
+  ∀ i, is_integral R ((B.basis.repr (x ^ n) i)) :=
+begin
+  by_cases htriv : nontrivial A, swap,
+  { intro i,
+    rw [subsingleton_iff.1 (not_nontrivial_iff_subsingleton.1 htriv) (x ^ n) 0],
+    simp [is_integral_zero] },
+  letI := htriv,
+  revert hx,
+  refine nat.case_strong_induction_on n _ (λ n hn, _),
+  { intros hx i,
+    rw [pow_zero, ← pow_zero B.gen, ← fin.coe_mk B.dim_pos, ← B.basis_eq_pow,
+      B.basis.repr_self_apply],
+    by_cases hi : (⟨0, B.dim_pos⟩ : fin _) = i,
+    { simp [hi, is_integral_one] },
+    { simp [hi, is_integral_zero] } },
+  { intros hx i,
+    rw [pow_succ],
+    refine is_integral_coord_prod hB hx (λ _, hn _ le_rfl (λ _, hx _) _) hmin _ }
 end
 
 lemma adjoin_gen_eq_top (B : power_basis R S) : adjoin R ({B.gen} : set S) = ⊤ :=
@@ -77,7 +101,7 @@ end
 lemma adjoin_eq_top_of_gen_mem {B : power_basis R S} {x : S} (hx : B.gen ∈ adjoin R ({x} : set S)) :
   adjoin R ({x} : set S) = ⊤ :=
 begin
-  rw [_root_.eq_top_iff, ← adjoin_gen_eq_top B],
+  rw [_root_.eq_top_iff, ← B.adjoin_gen_eq_top],
   refine adjoin_le _,
   simp [hx],
 end
@@ -92,7 +116,18 @@ lemma of_mem_adjon_gen {x : S} (hint : is_integral K x)
 
 variables [algebra R K] [is_scalar_tower R K S] {B}
 
-lemma to_matrix_is_integral_coeff {B' : power_basis K S} {P : R[X]} (h : aeval B.gen P = B'.gen) :
-  ∀ i j, _root_.is_integral R (B.basis.to_matrix B'.basis i j) := sorry
+lemma to_matrix_is_integral_coeff {B' : power_basis K S} {P : R[X]} (h : aeval B.gen P = B'.gen)
+  (hB : is_integral R B.gen) (hmin : minpoly K B.gen = (minpoly R B.gen).map (algebra_map R K)) :
+  ∀ i j, _root_.is_integral R (B.basis.to_matrix B'.basis i j) :=
+begin
+  intros i j,
+  rw [B.basis.to_matrix_apply, B'.coe_basis],
+  refine is_integral_coord_pow hB (λ i, _) hmin _ _,
+  rw [← h, aeval_eq_sum_range, linear_equiv.map_sum, finset.sum_apply'],
+  refine is_integral.sum _ (λ n hn, _),
+  rw [algebra.smul_def, is_scalar_tower.algebra_map_apply R K S, ← algebra.smul_def,
+    linear_equiv.map_smul, algebra_map_smul],
+  exact is_integral_smul _ (is_integral_coord_gen_pow hB hmin _ _),
+end
 
 end power_basis
