77012561636ddec97277c0dd1f5c41c0de078812
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-02-24 15:11:24 +0100

    add is_integral_of_aeval

--- src/ready_for_mathlib/integral_closure.lean
+++ src/ready_for_mathlib/integral_closure.lean
@@ -2,10 +2,19 @@ import ring_theory.integral_closure
 
 universes u v
 
-variables {R : Type u} {S : Type v} [comm_ring R] [comm_ring S] [algebra R S]
+variables {R : Type u} {A : Type v} [comm_ring R] [comm_ring A] [algebra R A]
 
-lemma is_integral_smul {x : S} (r : R) (hx : is_integral R x) : is_integral R (r • x) :=
+open polynomial
+
+lemma is_integral_smul {x : A} (r : R) (hx : is_integral R x) : is_integral R (r • x) :=
 begin
   rw [algebra.smul_def],
   refine is_integral_mul is_integral_algebra_map hx,
+end
+
+lemma is_integral_of_aeval {a  : A} {P : polynomial R} (ha : is_integral R a) :
+  is_integral R (aeval a P) :=
+begin
+  rw [aeval_eq_sum_range],
+  exact is_integral.sum _ (λ i _, is_integral_smul _ (is_integral.pow ha _)),
 end
