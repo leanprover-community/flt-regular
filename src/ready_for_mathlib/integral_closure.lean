95984569b447e49896de7cf32b0ce7d9c049ad4f
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-02-24 15:46:58 +0100

    generalize

--- src/ready_for_mathlib/integral_closure.lean
+++ src/ready_for_mathlib/integral_closure.lean
@@ -1,20 +1,26 @@
 import ring_theory.integral_closure
 
-universes u v
+universes u v w
 
-variables {R : Type u} {A : Type v} [comm_ring R] [comm_ring A] [algebra R A]
+variables {R : Type u} {S : Type w} {A : Type v}
+variables [comm_ring R] [comm_ring S] [comm_ring A]
+variables [algebra R S] [algebra R A] [algebra S A] [is_scalar_tower R S A]
 
 open polynomial
 
-lemma is_integral_smul {x : A} (r : R) (hx : is_integral R x) : is_integral R (r • x) :=
+open_locale polynomial
+
+lemma is_integral_smul {x : A} (r : R) (hx : is_integral S x) : is_integral S (r • x) :=
 begin
-  rw [algebra.smul_def],
+  rw [algebra.smul_def, is_scalar_tower.algebra_map_apply R S A],
   refine is_integral_mul is_integral_algebra_map hx,
 end
 
-lemma is_integral_of_aeval {a  : A} (P : polynomial R) (ha : is_integral R a) :
-  is_integral R (aeval a P) :=
+lemma is_integral_of_aeval {a  : A} (P : R[X]) (ha : is_integral S a) :
+  is_integral S (aeval a P) :=
 begin
-  rw [aeval_eq_sum_range],
-  exact is_integral.sum _ (λ i _, is_integral_smul _ (is_integral.pow ha _)),
+  rw [is_scalar_tower.aeval_apply R S A, aeval_eq_sum_range],
+  refine is_integral.sum _ (λ i _, _),
+  rw [coeff_map],
+  exact is_integral_smul _ (is_integral.pow ha _),
 end
