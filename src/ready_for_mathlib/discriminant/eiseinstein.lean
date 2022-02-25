0066eec3dc63d6bbddc8dcf3de54fbfbc6b7f6a6
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-02-25 15:27:03 +0100

    let's start this

--- src/ready_for_mathlib/discriminant/eiseinstein.lean
+++ src/ready_for_mathlib/discriminant/eiseinstein.lean
@@ -1,7 +1,7 @@
 import ring_theory.polynomial.eisenstein
 import ring_theory.polynomial.cyclotomic.basic
 
-variables {p : ℕ} (hp : p.prime)
+variables {p : ℕ} [hp : fact p.prime]
 
 open_locale polynomial
 
@@ -9,9 +9,15 @@ namespace polynomial
 
 local notation `𝓟` := submodule.span ℤ {p}
 
+include hp
+
 lemma cyclotomic.comp_is_eisenstein_at : ((cyclotomic p ℤ).comp (X + 1)).is_eisenstein_at 𝓟 :=
 { leading := sorry,
-  mem := sorry,
+  mem := λ i hi,
+  begin
+    rw [nat_degree_comp, show (X + 1 : ℤ[X]) = X + C 1, by simp, nat_degree_X_add_C, mul_one] at hi,
+    sorry
+  end,
   not_mem := sorry }
 
 end polynomial
