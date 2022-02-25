9fd0d3993d083c9e14446970915b1f401c4b6c04
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-02-25 14:38:59 +0100

    add lemmas

--- src/ready_for_mathlib/polynomial.lean
+++ src/ready_for_mathlib/polynomial.lean
@@ -14,4 +14,20 @@ begin
   rw [monic.def, leading_coeff_comp h, monic.def.1 hp, monic.def.1 hq, one_pow, one_mul],
 end
 
+lemma monic_comp_X_sub_C [is_domain R] {q : R[X]} (hq : q.monic) (r : R) :
+  (q.comp (X - C r)).monic :=
+begin
+  refine monic_comp hq (monic_X_sub_C _) (λ ha, _),
+  rw [nat_degree_X_sub_C] at ha,
+  exact one_ne_zero ha
+end
+
+lemma monic_comp_X_add_C [is_domain R] {q : R[X]} (hq : q.monic) (r : R) :
+  (q.comp (X + C r)).monic :=
+begin
+  refine monic_comp hq (monic_X_add_C _) (λ ha, _),
+  rw [nat_degree_X_add_C] at ha,
+  exact one_ne_zero ha
+end
+
 end polynomial
