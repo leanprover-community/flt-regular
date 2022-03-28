0bb68d7c34f558b42451e5f366af3403bd8e1bda
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-03-28 23:33:38 +0200

    discr_odd_prime_pow proved

--- src/ready_for_mathlib/discr.lean
+++ src/ready_for_mathlib/discr.lean
@@ -12,15 +12,13 @@ namespace is_cyclotomic_extension
 local attribute [instance] is_cyclotomic_extension.finite_dimensional
 local attribute [instance] is_cyclotomic_extension.is_galois
 
-/-- If `p` is an odd prime and `is_cyclotomic_extension {p} K L`, then
-`discr K (hζ.power_basis K).basis = (-1) ^ ((p - 1) / 2) * p ^ (p - 2)`. -/
 lemma discr_odd_prime_pow [is_cyclotomic_extension {p ^ (k + 1)} K L] [hp : fact (p : ℕ).prime]
   (hζ : is_primitive_root ζ ↑(p ^ (k + 1))) (hirr : irreducible (cyclotomic (↑(p ^ (k + 1)) : ℕ) K))
+  (hirr₁ : irreducible (cyclotomic (p : ℕ) K))
   (hodd : p ≠ 2) : discr K (hζ.power_basis K).basis =
-  (-1) ^ (((p : ℕ) - 1) / 2) * p ^ ((p : ℕ) - 2) :=
+  (-1) ^ (((p : ℕ) - 1) / 2) * p ^ ((p : ℕ) ^ k * ((p - 1) * (k + 1) - 1)) :=
 begin
   have hodd' : (p : ℕ) ≠ 2 := λ hn, by exact hodd.symm (pnat.coe_inj.1 hn.symm),
-  --have hpos := pos_iff_ne_zero.2 (λ h, (tsub_pos_of_lt (prime.one_lt hp.out)).ne.symm h),
   haveI : ne_zero ((↑(p ^ (k + 1)) : ℕ) : K),
   { refine ⟨λ hzero, _⟩,
     rw [pnat.pow_coe] at hzero,
@@ -44,24 +42,34 @@ begin
         { refine pow_ne_zero _ (hp.out.ne_zero) } },
       { left,
         exact nat.even_sub_one_of_prime_ne_two hp.out hodd' } } },
-      {
-        sorry
-      },
+      { have H := congr_arg derivative (cyclotomic_prime_pow_mul_X_pow_sub_one K p k),
+        rw [derivative_mul, derivative_sub, derivative_one, sub_zero, derivative_pow,
+          derivative_X, mul_one, derivative_sub, derivative_one, sub_zero, derivative_pow,
+          derivative_X, mul_one, ← pnat.pow_coe, hζ.minpoly_eq_cyclotomic_of_irreducible hirr] at H,
+        replace H := congr_arg (λ P, aeval ζ P) H,
+        simp only [aeval_add, aeval_mul, minpoly.aeval, zero_mul, add_zero, aeval_nat_cast,
+          _root_.map_sub, aeval_one, aeval_X_pow] at H,
+        replace H := congr_arg (algebra.norm K) H,
+        rw [monoid_hom.map_mul, hζ.pow_prime_ne_two_pow_sub_one_norm hirr
+          (by simpa [tsub_self, pow_one] using hirr₁) hodd rfl.le, monoid_hom.map_mul,
+          ← map_nat_cast (algebra_map K L), norm_algebra_map, finrank _ hirr, pnat.pow_coe,
+          totient_prime_pow hp.out (succ_pos k), nat.sub_one, nat.pred_succ,
+          ← hζ.minpoly_eq_cyclotomic_of_irreducible hirr, map_pow, hζ.norm_eq_one _ hirr, one_pow,
+          mul_one, cast_pow, ← coe_coe, ← pow_mul, ← mul_assoc, mul_comm (k + 1), mul_assoc] at H,
+        { have := mul_pos (succ_pos k) (tsub_pos_iff_lt.2 hp.out.one_lt),
+          rw [← succ_pred_eq_of_pos this, mul_succ, pow_add _ _ ((p : ℕ) ^ k)] at H,
+          replace H := (mul_left_inj' (λ h, _)).1 H,
+          { simpa only [← pnat.pow_coe, H, mul_comm _ (k + 1)] },
+          { replace h := pow_eq_zero h,
+            rw [coe_coe] at h,
+            exact ne_zero.ne _ h } },
+        { intro h,
+          rw [← pnat.coe_inj, pnat.coe_bit0, pnat.one_coe, pnat.pow_coe, ← pow_one 2] at h,
+          replace h := eq_of_prime_pow_eq (prime_iff.1 hp.out) (prime_iff.1 nat.prime_two)
+            (k.succ_pos) h,
+          exact hodd' h },
+        apply_instance },
       { apply_instance }
-  -- { have H := congr_arg derivative (cyclotomic_prime_mul_X_sub_one K p),
-  --   rw [derivative_mul, derivative_sub, derivative_one, derivative_X, sub_zero, mul_one,
-  --     derivative_sub, derivative_one, sub_zero, derivative_X_pow] at H,
-  --   replace H := congr_arg (λ P, aeval ζ P) H,
-  --   simp only [hζ.minpoly_eq_cyclotomic_of_irreducible hirr, aeval_add, _root_.map_mul, aeval_one,
-  --     _root_.map_sub, aeval_X, minpoly.aeval, add_zero, aeval_nat_cast, aeval_X_pow] at H,
-  --   replace H := congr_arg (algebra.norm K) H,
-  --   rw [monoid_hom.map_mul, hζ.sub_one_norm_prime hirr hodd, monoid_hom.map_mul, monoid_hom.map_pow,
-  --     hζ.norm_eq_one hodd hirr, one_pow, mul_one, ← map_nat_cast (algebra_map K L),
-  --     norm_algebra_map, finrank _ hirr, totient_prime hp.out, ← succ_pred_eq_of_pos hpos, pow_succ,
-  --     mul_comm _ (p : K), coe_coe, ← hζ.minpoly_eq_cyclotomic_of_irreducible hirr] at H,
-  --   simpa [(mul_right_inj' $ ne_zero.ne ↑↑p).1 H],
-  --   apply_instance },
-  -- { apply_instance },
 end
 
 end is_cyclotomic_extension
