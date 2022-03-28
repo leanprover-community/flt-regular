cafa5681d7f967e0bfc0b15352bfc3942d2522d0
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-03-28 18:29:50 +0200

    start proof of discr_odd_prime_pow

---
+++ src/ready_for_mathlib/discr.lean
@@ -0,0 +1,67 @@
+import ready_for_mathlib.primitive_root
+import ring_theory.discriminant
+
+universes u v
+variables {p : ℕ+} (k : ℕ) {K : Type u} {L : Type v} {ζ : L} [field K] [field L]
+variables [algebra K L] [ne_zero ((p : ℕ) : K)]
+
+open algebra polynomial nat is_primitive_root
+
+namespace is_cyclotomic_extension
+
+local attribute [instance] is_cyclotomic_extension.finite_dimensional
+local attribute [instance] is_cyclotomic_extension.is_galois
+
+/-- If `p` is an odd prime and `is_cyclotomic_extension {p} K L`, then
+`discr K (hζ.power_basis K).basis = (-1) ^ ((p - 1) / 2) * p ^ (p - 2)`. -/
+lemma discr_odd_prime_pow [is_cyclotomic_extension {p ^ (k + 1)} K L] [hp : fact (p : ℕ).prime]
+  (hζ : is_primitive_root ζ ↑(p ^ (k + 1))) (hirr : irreducible (cyclotomic (↑(p ^ (k + 1)) : ℕ) K))
+  (hodd : p ≠ 2) : discr K (hζ.power_basis K).basis =
+  (-1) ^ (((p : ℕ) - 1) / 2) * p ^ ((p : ℕ) - 2) :=
+begin
+  have hodd' : (p : ℕ) ≠ 2 := λ hn, by exact hodd.symm (pnat.coe_inj.1 hn.symm),
+  --have hpos := pos_iff_ne_zero.2 (λ h, (tsub_pos_of_lt (prime.one_lt hp.out)).ne.symm h),
+  haveI : ne_zero ((↑(p ^ (k + 1)) : ℕ) : K),
+  { refine ⟨λ hzero, _⟩,
+    rw [pnat.pow_coe] at hzero,
+    simpa [ne_zero.ne ((p : ℕ) : K)] using hzero },
+
+  rw [discr_power_basis_eq_norm, finrank _ hirr, hζ.power_basis_gen _,
+    ← hζ.minpoly_eq_cyclotomic_of_irreducible hirr, pnat.pow_coe, totient_prime_pow hp.out
+    (succ_pos k)],
+  congr' 1,
+  { have h := even_sub_one_of_prime_ne_two hp.out hodd',
+    rw [← mul_one 2, ← mul_comm ((p : ℕ) - 1), mul_assoc ((p : ℕ) - 1), ← nat.div_mul_div_comm
+      (even_iff_two_dvd.1 h) (one_dvd _), nat.div_one, mul_one, pow_mul, nat.sub_one (k + 1),
+      nat.pred_succ],
+    cases neg_one_pow_eq_or K (((p : ℕ) - 1) / 2) with hpos hneg,
+    { rw [hpos, one_pow] },
+    { rw [hneg],
+      refine nat.neg_one_pow_of_odd (nat.odd_mul.2 ⟨odd.pow (decidable.or_iff_not_imp_left.1
+        (hp.out.eq_two_or_odd') hodd'), nat.even.sub_odd _ (even_mul.2 _) odd_one⟩),
+      { refine one_le_iff_ne_zero.2 (nat.mul_ne_zero (λ h, _) _),
+        { exact (not_lt_of_le (tsub_eq_zero_iff_le.1 h)) (prime.one_lt hp.out) },
+        { refine pow_ne_zero _ (hp.out.ne_zero) } },
+      { left,
+        exact nat.even_sub_one_of_prime_ne_two hp.out hodd' } } },
+      {
+        sorry
+      },
+      { apply_instance }
+  -- { have H := congr_arg derivative (cyclotomic_prime_mul_X_sub_one K p),
+  --   rw [derivative_mul, derivative_sub, derivative_one, derivative_X, sub_zero, mul_one,
+  --     derivative_sub, derivative_one, sub_zero, derivative_X_pow] at H,
+  --   replace H := congr_arg (λ P, aeval ζ P) H,
+  --   simp only [hζ.minpoly_eq_cyclotomic_of_irreducible hirr, aeval_add, _root_.map_mul, aeval_one,
+  --     _root_.map_sub, aeval_X, minpoly.aeval, add_zero, aeval_nat_cast, aeval_X_pow] at H,
+  --   replace H := congr_arg (algebra.norm K) H,
+  --   rw [monoid_hom.map_mul, hζ.sub_one_norm_prime hirr hodd, monoid_hom.map_mul, monoid_hom.map_pow,
+  --     hζ.norm_eq_one hodd hirr, one_pow, mul_one, ← map_nat_cast (algebra_map K L),
+  --     norm_algebra_map, finrank _ hirr, totient_prime hp.out, ← succ_pred_eq_of_pos hpos, pow_succ,
+  --     mul_comm _ (p : K), coe_coe, ← hζ.minpoly_eq_cyclotomic_of_irreducible hirr] at H,
+  --   simpa [(mul_right_inj' $ ne_zero.ne ↑↑p).1 H],
+  --   apply_instance },
+  -- { apply_instance },
+end
+
+end is_cyclotomic_extension
