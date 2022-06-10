050150b5cbe80c4f1689a725288a3a347ff78e84
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-06-10 12:57:56 +0200

    refactor gal_conj

--- src/number_theory/cyclotomic/galois_action_on_cyclo.lean
+++ src/number_theory/cyclotomic/galois_action_on_cyclo.lean
@@ -2,25 +2,23 @@ import number_theory.cyclotomic.cyclotomic_units
 import number_theory.cyclotomic.number_field_embeddings
 import number_theory.cyclotomic.absolute_value
 
-import number_theory.cyclotomic.basic
+import number_theory.cyclotomic.gal
 
 universes u
 
 open finite_dimensional
 
-variables (p : ℕ+)
+variables (K : Type*) (p : ℕ+) [field K] [char_zero K] [is_cyclotomic_extension {p} ℚ K]
+variables {ζ : K} (hζ : is_primitive_root ζ p)
 
-local notation `KK` := cyclotomic_field p ℚ
+--local notation `KK` := cyclotomic_field p ℚ
 
-local notation `RR` := number_field.ring_of_integers (cyclotomic_field p ℚ)
-
-local attribute [instance] is_cyclotomic_extension.number_field is_cyclotomic_extension.finite_dimensional
+local notation `RR` := number_field.ring_of_integers K
 
 -- @Chris: you mentioned "checking automorphisms agree only on a generator" -
 -- what you want is `power_basis.alg_hom_ext`
 
-open_locale number_field
-
+open_locale number_field cyclotomic
 
 open cyclotomic_ring embeddings
 
@@ -28,69 +26,23 @@ noncomputable theory
 open is_cyclotomic_extension
 open polynomial
 
-local notation `ζ` := zeta p ℚ KK
-
-@[simp]
-lemma minpoly_zeta : minpoly ℚ ζ = cyclotomic p ℚ :=
-begin
-  rw ← map_cyclotomic_int,
-  have : is_primitive_root ζ p := zeta_primitive_root p ℚ (cyclotomic_field p ℚ),
-  rw cyclotomic_eq_minpoly this p.pos,
-  have : is_integral ℤ ζ,
-  from is_primitive_root.is_integral this p.pos,
-  have h_mins : minpoly ℚ ζ = (minpoly ℤ ζ).map (algebra_map ℤ ℚ),
-  from minpoly.gcd_domain_eq_field_fractions ℚ this,
-  rw h_mins,
-  refl,
-end
-
--- a version of cyclotomic_eq_minpoly
-lemma minpoly_primitive_root {z : KK} (h : is_primitive_root z p) : minpoly ℚ z = cyclotomic p ℚ :=
-begin
-  rw ← map_cyclotomic_int,
-  rw cyclotomic_eq_minpoly h p.pos,
-  have : is_integral ℤ z,
-  from is_primitive_root.is_integral h p.pos,
-  have h_mins : minpoly ℚ z = (minpoly ℤ z).map (algebra_map ℤ ℚ),
-  from minpoly.gcd_domain_eq_field_fractions ℚ this,
-  rw h_mins,
-  refl,
-end
-
-/-- The power basis of the p-th cyclotomic field given by the chosen pth root of unity. -/
-def power_basis_zeta_runity : power_basis ℚ KK :=
-{ gen := zeta p ℚ KK,
-  dim := (minpoly ℚ (zeta p ℚ (cyclotomic_field p ℚ))).nat_degree,
-  basis := basis.mk (is_integral.linear_independent_pow
-      (is_separable.is_integral ℚ (zeta p ℚ (cyclotomic_field p ℚ)))) sorry,
-  basis_eq_pow := by simp }
-
-@[simp]
-lemma power_basis_zeta_runity_gen : (power_basis_zeta_runity p).gen = ζ := rfl
+--local notation `ζ` := zeta p ℚ KK
 
 /-- complex conjugation as a Galois automorphism -/
-def gal_conj : KK →ₐ[ℚ] KK := power_basis.lift (power_basis_zeta_runity p) ζ⁻¹
-begin
-  simp only [power_basis_zeta_runity_gen, minpoly_zeta],
-  have : is_primitive_root ζ p,
-  from zeta_primitive_root p ℚ (cyclotomic_field p ℚ),
-  have : is_primitive_root ζ⁻¹ p,
-  exact is_primitive_root.inv' this,
-  rw ← minpoly_primitive_root _ this,
-  exact minpoly.aeval _ _,
-end
+def gal_conj : K →ₐ[ℚ] K :=
+((is_cyclotomic_extension.aut_equiv_pow _ (cyclotomic.irreducible_rat p.pos)).symm (-1)).to_alg_hom
+
+variables {K p}
 
 @[simp]
-lemma gal_conj_zeta_runity : gal_conj p ζ = ζ⁻¹ := power_basis.lift_gen _ _ _
+lemma gal_conj_zeta_runity : gal_conj K p ζ = ζ⁻¹ := sorry
 
-lemma gal_conj_zeta_runity_pow (n : ℕ) :  gal_conj p (ζ^n) = (ζ⁻¹)^n :=
+lemma gal_conj_zeta_runity_pow (n : ℕ) :  gal_conj K p (ζ^n) = (ζ⁻¹)^n :=
 begin
-induction n,
-simp only [alg_hom.map_one, pow_zero],
-simp only [alg_hom.map_pow, gal_conj_zeta_runity],
+sorry
 end
 
-lemma gal_conj_zeta_runity_coe : gal_conj p (ζ) =  gal_conj p (ζ : KK) :=
+lemma gal_conj_zeta_runity_coe : gal_conj K p (ζ) =  gal_conj K p (ζ : K) :=
 begin
 refl,
 end
@@ -99,21 +51,23 @@ open_locale complex_conjugate
 
 lemma conj_norm_one (x : ℂ) (h : complex.abs x = 1) : conj x = x⁻¹ := sorry
 
+include hζ
+
 @[simp]
-lemma embedding_conj (x : KK) (φ : KK →+* ℂ) : conj (φ x) = φ (gal_conj p x) :=
+lemma embedding_conj (x : K) (φ : K →+* ℂ) : conj (φ x) = φ (gal_conj K p x) :=
 begin
   swap,
   revert x,
-  suffices : φ (gal_conj p ζ) = conj (φ ζ),
+  suffices : φ (gal_conj K p ζ) = conj (φ ζ),
   sorry, -- this should be a general lemma about checking automorphisms agree only on a generator
   rw conj_norm_one,
   simp [ring_hom.map_inv],
   sorry,
 end
 
-lemma gal_conj_idempotent : (gal_conj p).comp (gal_conj p) = (alg_hom.id _ _) :=
+lemma gal_conj_idempotent : (gal_conj K p).comp (gal_conj K p) = (alg_hom.id ℚ K) :=
 begin
-  suffices : (gal_conj p ∘ gal_conj p) ζ = (alg_hom.id ℚ _) ζ,
+  suffices : (gal_conj K p ∘ gal_conj K p) ζ = (alg_hom.id ℚ _) ζ,
   sorry, -- this should be a general lemma about checking automorphisms agree only on a generator
   simp,
 end
@@ -121,9 +75,10 @@ end
 -- TODO we should generlize this and have a way to automatically transfer galois automorphisms
 -- to automorphisms of the unit group
 /-- The conjugate as a map from units to itself -/
+--Do we have that an alg automorphism presevers algebraic integers?
 def unit_gal_conj : RRˣ → RRˣ :=
 λ ⟨u_val, u_inv, u_val_inv, u_inv_val⟩,
-  ⟨⟨gal_conj p u_val, sorry⟩, ⟨gal_conj p u_inv, sorry⟩,
+  ⟨⟨gal_conj K p u_val, sorry⟩, ⟨gal_conj K p u_inv, sorry⟩,
   begin
     ext,
     simp only [subalgebra.coe_one, set_like.coe_mk, subalgebra.coe_mul],
@@ -138,26 +93,26 @@ def unit_gal_conj : RRˣ → RRˣ :=
 
 /-- `unit_gal_conj` as a bundled hom. -/
 def unit_gal_conj_m : RRˣ →* RRˣ :={
-  to_fun := unit_gal_conj  p,
+  to_fun := unit_gal_conj hζ,
   map_one' := by {simp_rw (unit_gal_conj ),  sorry, },
   map_mul' := by {sorry,},
  }
 
-lemma unit_gal_conj_spec (u : RRˣ) : gal_conj p u = unit_gal_conj p u :=
+lemma unit_gal_conj_spec (u : RRˣ) : gal_conj K p u = unit_gal_conj hζ u :=
 begin
   cases u,
   simp [unit_gal_conj],
 end
 
-lemma uni_gal_conj_inv (u : RRˣ) : (unit_gal_conj p u)⁻¹ = (unit_gal_conj p u⁻¹) :=
+lemma uni_gal_conj_inv (u : RRˣ) : (unit_gal_conj hζ u)⁻¹ = (unit_gal_conj hζ u⁻¹) :=
 begin
 rw unit_gal_conj,
 simp,
 sorry,
 end
 
-lemma unit_lemma_val_one (u : RRˣ) (φ : KK →+* ℂ) :
-  complex.abs (φ (u * (unit_gal_conj p u)⁻¹)) = 1 :=
+lemma unit_lemma_val_one (u : RRˣ) (φ : K →+* ℂ) :
+  complex.abs (φ (u * (unit_gal_conj hζ u)⁻¹)) = 1 :=
 begin
   rw ring_hom.map_mul,
   rw complex.abs.is_absolute_value.abv_mul,
@@ -166,11 +121,13 @@ begin
   rw ← unit_gal_conj_spec,
   rw ← embedding_conj,
   simp [-embedding_conj],
+  sorry,
+  sorry,
 end
 
-lemma unit_gal_conj_idempotent (u : RRˣ) : (unit_gal_conj p (unit_gal_conj p u)) = u :=
+lemma unit_gal_conj_idempotent (u : RRˣ) : (unit_gal_conj hζ (unit_gal_conj hζ u)) = u :=
 begin
-   have:=  (unit_gal_conj_spec p u),
+   have:=  (unit_gal_conj_spec hζ u),
    simp at this,
    sorry,
 end
