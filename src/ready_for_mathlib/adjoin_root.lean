df5e8d70568324a00495ec85dad7a8b8ca26d371
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-06-26 16:35:34 +0200

    K is useless

--- src/ready_for_mathlib/adjoin_root.lean
+++ src/ready_for_mathlib/adjoin_root.lean
@@ -1,6 +1,7 @@
 import ring_theory.adjoin_root
+import ready_for_mathlib.minpoly
 
-variables {R A B K : Type*}
+variables {R A B : Type*}
 
 namespace alg_hom
 
@@ -15,7 +16,7 @@ algebra.eq_top_iff --I think this formulation can be useful `eq_top_iff` doesn't
 end alg_hom
 
 variables (R) [comm_ring R] [comm_ring A] [algebra R A] (x : A)
-variables [field K] [algebra R K] [is_fraction_ring R K] [algebra K A] [is_scalar_tower R K A]
+variables (hinj : function.injective (algebra_map R A))
 
 noncomputable theory
 
@@ -49,15 +50,13 @@ begin
   refine ⟨mk (minpoly R x) X, by simpa using h.symm⟩
 end
 
-variables (K) {R} {x} [is_domain R] [normalized_gcd_monoid R] [is_domain A]
+variables {R} {x} [is_domain R] [normalized_gcd_monoid R] [is_domain A]
 
-include K
+include hinj
 
 lemma minpoly.to_adjoin.injective (hx : is_integral R x) :
   function.injective (minpoly.to_adjoin R x) :=
 begin
-  have hinj : function.injective (algebra_map R A) := (is_scalar_tower.algebra_map_eq R K A).symm ▸
-    (algebra_map K A).injective.comp (is_fraction_ring.injective R K),
   refine (injective_iff_map_eq_zero _).2 (λ P₁ hP₁, _),
   obtain ⟨P, hP⟩ := mk_surjective (minpoly.monic hx) P₁,
   by_cases hPzero : P = 0,
@@ -66,48 +65,48 @@ begin
   rw [← hP, minpoly.to_adjoin_apply, lift_hom_mk, ← subalgebra.coe_eq_zero,
     aeval_subalgebra_coe, set_like.coe_mk, P.eq_C_content_mul_prim_part, aeval_mul, aeval_C] at hP₁,
   replace hP₁ := eq_zero_of_ne_zero_of_mul_left_eq_zero ((map_ne_zero_iff _ hinj).2 hPcont) hP₁,
-  obtain ⟨Q, hQ⟩ := minpoly.gcd_domain_dvd K hx P.is_primitive_prim_part hP₁,
-  rw [P.eq_C_content_mul_prim_part, map_mul, hQ, map_mul, mk_self, zero_mul, mul_zero] at hP,
-  exact hP.symm
+  obtain ⟨Q, hQ⟩ := minpoly.gcd_domain_dvd' hx hinj P.is_primitive_prim_part.ne_zero hP₁,
+  rw [P.eq_C_content_mul_prim_part] at hP,
+  simpa [hQ] using hP.symm
 end
 
 /-- The algebra isomorphism `adjoin_root (minpoly R x) ≃ₐ[R] adjoin R x` -/
 @[simps] def minpoly.to_adjoin_equiv (hx : is_integral R x) :
   adjoin_root (minpoly R x) ≃ₐ[R] adjoin R ({x} : set A) :=
 alg_equiv.of_bijective (minpoly.to_adjoin R x)
-  ⟨minpoly.to_adjoin.injective K hx, minpoly.to_adjoin.surjective R x⟩
+  ⟨minpoly.to_adjoin.injective hinj hx, minpoly.to_adjoin.surjective R x⟩
 
 end adjoin_root
 
-namespace algebra
-
 open adjoin_root
 
-variables (K) {R} {x} [is_domain R] [normalized_gcd_monoid R] [is_domain A]
+namespace algebra
 
-include K
+variables {R} {x} [is_domain R] [normalized_gcd_monoid R] [is_domain A]
+
+include hinj
 
 /-- The `power_basis` of `adjoin R {x}` given by `x`. -/
 @[simps] def adjoin.power_basis' (hx : _root_.is_integral R x) :
   power_basis R (adjoin R ({x} : set A)) :=
-power_basis.map (adjoin_root.power_basis' (minpoly.monic hx)) (minpoly.to_adjoin_equiv K hx)
+power_basis.map (adjoin_root.power_basis' (minpoly.monic hx)) (minpoly.to_adjoin_equiv hinj hx)
 
 lemma adjoin.power_basis_gen' (hx : _root_.is_integral R x) :
-  (adjoin.power_basis' K hx).gen = ⟨x, self_mem_adjoin_singleton R x⟩ :=
+  (adjoin.power_basis' hinj hx).gen = ⟨x, self_mem_adjoin_singleton R x⟩ :=
 by simp
 
 end algebra
 
 namespace power_basis
 
-variables (K) {R} {x} [is_domain R] [normalized_gcd_monoid R] [is_domain A]
+variables {R} {x} [is_domain R] [normalized_gcd_monoid R] [is_domain A]
 
-include K
+include hinj
 
 /-- The power basis given by `x` if `B.gen ∈ adjoin R {x}`. -/
 @[simps] noncomputable def of_gen_mem_adjoin' (B : power_basis R A) (hint : is_integral R x)
   (hx : B.gen ∈ adjoin R ({x} : set A)) : power_basis R A :=
-(algebra.adjoin.power_basis' K hint).map $
+(algebra.adjoin.power_basis' hinj hint).map $
   (subalgebra.equiv_of_eq _ _ $ power_basis.adjoin_eq_top_of_gen_mem_adjoin hx).trans
   subalgebra.top_equiv
 
