b0476c8e7a220a5fed7a696c4245fcd38471be16
Eric Rodriguez <ericrboidi@gmail.com>
2022-07-12 11:20:44 +0100

    bump

--- src/number_theory/cyclotomic/class_group.lean
+++ src/number_theory/cyclotomic/class_group.lean
@@ -7,15 +7,15 @@ variable (n : ℕ+)
 
 section finiteness
 
+instance : is_integral_closure (cyclotomic_ring n ℤ ℚ) ℤ (cyclotomic_field n ℚ) := sorry
+
 local attribute [instance] is_cyclotomic_extension.finite_dimensional
 
 -- todo: we should change this definition to not use `cyclotomic_ring`.
 
 noncomputable instance class_group.fintype_of_cyclotomic_ring (n : ℕ+) :
   fintype (class_group (cyclotomic_ring n ℤ ℚ) (cyclotomic_field n ℚ)) :=
-sorry
-
-/- class_group.fintype_of_admissible_of_finite ℚ (cyclotomic_field n ℚ)
-  absolute_value.abs_is_admissible -/
+class_group.fintype_of_admissible_of_finite ℚ (cyclotomic_field n ℚ)
+  absolute_value.abs_is_admissible
 
 end finiteness
