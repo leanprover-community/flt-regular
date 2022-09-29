25d77279766987485b3896942ea92d99b1694300
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-09-29 10:58:48 +0200

    better

--- src/ready_for_mathlib/basis.lean
+++ src/ready_for_mathlib/basis.lean
@@ -4,8 +4,7 @@ variables {R M ι : Type*} [semiring R] [add_comm_monoid M] [module R M]
 
 namespace basis
 
-lemma coord_dvd_of_dvd (b : basis ι R M) (i : ι) {m₁ m₂ : M} {r : R} (h : m₂ = r • m₁) :
-  r ∣ b.coord i m₂ :=
-⟨b.coord i m₁, by simp [h]⟩
+lemma coord_dvd_of_dvd (b : basis ι R M) (i : ι) (m : M) (r : R) : r ∣ b.coord i (r • m) :=
+⟨b.coord i m, by simp⟩
 
 end basis
