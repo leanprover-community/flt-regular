912d42ecbee4aece5c80bb0df2d12fdfc7014436
Riccardo Brasca <riccardo.brasca@gmail.com>
2022-10-04 13:55:04 +0200

    reorganize stuff

--- src/flt_regular.lean
+++ src/flt_regular.lean
@@ -1,72 +1,18 @@
-import ring_theory.class_group
-import number_theory.regular_primes
-import tactic.may_assume
-import data.zmod.extras
-import tactic
-import data.nat.prime_extras
-import algebra.gcd_monoid.nat
-import number_theory.cyclotomic.factoring
-import number_theory.cyclotomic.case_I
+import caseI.statement
+import caseII.statement
 
--- TODO I (alex) commented this out as it seems redundent now? -- agree, seems redundant - Eric
--- lemma flt_coprime (p a b c : ℕ) [fact p.prime] (h : a ^ p + b ^ p = c ^ p) (hab : a.coprime b)
---     : b.coprime c ∧ a.coprime c := admit
+open flt_regular
 
-open polynomial fractional_ideal
+/-- Statement of Fermat's last theorem for regular primes. -/
+def flt_regular.statement : Prop := ∀ ⦃a b c : ℤ⦄ ⦃p : ℕ⦄ (hpri : p.prime)
+  (hreg : is_regular_number p hpri.pos) (hodd : p ≠ 2) (hprod : a * b * c ≠ 0),
+  a ^ p + b ^ p ≠ c ^ p
 
-open_locale non_zero_divisors number_field
-
-/-- Case II (when a,b,c are not coprime to the exponent), of FLT for regular primes. -/
-theorem flt_regular_case_two (p a b c : ℕ) (hp : p.prime) (hpreg : is_regular_number p hp.pos)
-  (hpne_two : p ≠ 2) (h : a ^ p + b ^ p = c ^ p) (hpabc : p ∣ a * b * c) : a * b * c = 0 := sorry
-
-theorem flt_regular (p a b c : ℕ) (hp : p.prime) (hpreg : is_regular_number p hp.pos) (hpne_two : p ≠ 2)
-  (h : a ^ p + b ^ p = c ^ p) : a * b * c = 0 :=
+/-- Fermat's last theorem for regular primes.. -/
+theorem flt_regular {a b c : ℤ} {p : ℕ} (hpri : p.prime) (hreg : is_regular_number p hpri.pos)
+  (hodd : p ≠ 2) (hprod : a * b * c ≠ 0) : a ^ p + b ^ p ≠ c ^ p :=
 begin
-  sorry
-  -- may_assume hcoprime : ({a, b, c} : finset ℕ).gcd id = 1,
-  -- { let s : finset ℕ := {a, b, c},
-  --   let d : ℕ := s.gcd id,
-  --   rcases eq_or_ne c 0 with rfl | hc, --budget may_assume ;b needed for `image_div_gcd_coprime`
-  --   { rw mul_zero },
-  --   cases d.eq_zero_or_pos with hd hd,
-  --   { rw finset.gcd_eq_zero_iff at hd,
-  --     rw mul_eq_zero,
-  --     exact or.inr (hd c $ by simp) },
-  --   specialize h_red p (a/d) (b/d) (c/d) ‹_› hp hpne_two _ _,
-  --   { have habc := congr_arg (/ d^p) h,
-  --     simp only at habc,
-  --     rw nat.add_div (pow_pos hd p) at habc,
-  --     have : ite (d ^ p ≤ a ^ p % d ^ p + b ^ p % d ^ p) 1 0 = 0,
-  --     { simp only [nat.one_ne_zero, ite_eq_right_iff, imp_false, not_le],
-  --       convert pow_pos hd p,
-  --       rw add_eq_zero_iff,
-  --       split;
-  --       { apply nat.mod_eq_zero_of_dvd,
-  --         apply pow_dvd_pow_of_dvd,
-  --         apply finset.gcd_dvd,
-  --         simp } },
-  --     have key : ∀ x ∈ ({a, b, c} : finset ℕ), x ^ p / d ^ p = (x / d) ^ p,
-  --     { intros x xh,
-  --       rw div_pow''', -- TODO move this lemma to a reasonable place / name and mathlib
-  --       exact (finset.gcd_dvd xh), },
-  --     simpa only [this, key, true_or, eq_self_iff_true, or_true, finset.mem_insert,
-  --                 finset.mem_singleton] using habc },
-  --   { convert s.coprime_of_div_gcd _ hc using 1,
-  --     conv_rhs { rw finset.gcd_eq_gcd_image },
-  --     congr,
-  --     simp only [finset.image_insert, id.def, finset.image_singleton, normalize_eq],
-  --     simp },
-  --   { have habc := congr_arg (* d^3) h_red,
-  --     simp only [zero_mul] at habc,
-  --     replace habc : (a / d * d) * (b / d * d) * (c / d * d) = 0,
-  --     { convert habc using 1, ring },
-  --     iterate 3 { rw nat.div_mul_cancel at habc },
-  --     exact habc,
-  --     all_goals { apply finset.gcd_dvd, simp } } },
-  -- cases nat.coprime_or_dvd_of_prime (fact.out p.prime) (a * b * c) with hpabc hpabc,
-  -- { exact absurd hpabc (λ habs, sorry),
-  --    --exact absurd hpabc (λ _, flt_regular_case_one hp hpne_two sorry hpabc h)
-  --    },
-  -- { exact flt_regular_case_two p a b c hp hpne_two h hpabc }
+  by_cases case : ↑p ∣ a * b * c,
+  exact caseII hpri hreg hodd hprod case,
+  exact caseI hpri hreg hodd hprod case
 end
--- src/may_assume/lemmas.lean
+++ src/may_assume/lemmas.lean
@@ -137,44 +137,4 @@ begin
     exact caseI }
 end
 
-/-- Case I with additional assumptions. -/
-def caseI_slightly_easier : Prop := ∀ ⦃a b c : ℤ⦄ ⦃p : ℕ⦄ (hpri : p.prime)
-  (hreg : is_regular_number p hpri.pos) (hp5 : 5 ≤ p) (hprod : a * b * c ≠ 0)
-  (hgcd : is_unit (({a, b, c} : finset ℤ).gcd id))
-  (hab : ¬a ≡ b [ZMOD p]) (caseI : ¬ ↑p ∣ a * b * c), a ^ p + b ^ p ≠ c ^ p
-
-/-- Case I. -/
-def caseI : Prop := ∀ ⦃a b c : ℤ⦄ ⦃p : ℕ⦄ (hpri : p.prime) (hreg : is_regular_number p hpri.pos)
-  (hodd : p ≠ 2) (hprod : a * b * c ≠ 0) (caseI : ¬ ↑p ∣ a * b * c), a ^ p + b ^ p ≠ c ^ p
-
-lemma may_assume : caseI_slightly_easier → caseI :=
-begin
-  rw [caseI_slightly_easier, caseI],
-  intro Heasy,
-  intros a b c p hpri hreg hodd hprod hI H,
-  have hp5 : 5 ≤ p,
-  { by_contra' habs,
-    have : p ∈ finset.Ioo 2 5 := finset.mem_Icc.2 ⟨nat.lt_of_le_and_ne hpri.two_le hodd.symm,
-      by linarith⟩,
-    fin_cases this,
-    { exact may_assume.p_ne_three hprod H rfl },
-    { rw [show 4 = 2 * 2, from rfl] at hpri,
-      refine nat.not_prime_mul one_lt_two one_lt_two hpri } },
-  rcases may_assume.coprime H hprod with ⟨Hxyz, hunit, hprodxyx⟩,
-  let d := ({a, b, c} : finset ℤ).gcd id,
-  have hdiv : ¬↑p ∣ (a / d) * (b / d) * (c / d),
-  { have hadiv : d ∣ a := gcd_dvd (by simp),
-    have hbdiv : d ∣ b := gcd_dvd (by simp),
-    have hcdiv : d ∣ c := gcd_dvd (by simp),
-    intro hdiv,
-    replace hdiv := dvd_mul_of_dvd_right hdiv ((d * d) * d),
-    rw [mul_assoc, ← mul_assoc d, ← mul_assoc d, int.mul_div_cancel' hadiv, mul_assoc,
-      mul_comm a, mul_assoc (b / d), ← mul_assoc _ (b / d), int.mul_div_cancel' hbdiv,
-      mul_comm, mul_assoc, mul_assoc, int.div_mul_cancel hcdiv, mul_comm, mul_assoc,
-      mul_comm c, ← mul_assoc] at hdiv,
-    exact hI hdiv },
-  obtain ⟨X, Y, Z, H1, H2, H3, H4, H5⟩ := a_not_cong_b hpri hp5 hprodxyx Hxyz hunit hdiv,
-  exact Heasy hpri hreg hp5 H4 H2 H3 (λ hfin, H5 hfin) H1
-end
-
 end flt_regular
