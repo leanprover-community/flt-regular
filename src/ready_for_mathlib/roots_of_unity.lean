9c8ee0607645940eb232b355b82f87439acabdd5
riccardobrasca <riccardo.brasca@gmail.com>
2022-10-06 14:21:19 +0200

    add mem_nth_roots

---
+++ src/ready_for_mathlib/roots_of_unity.lean
@@ -0,0 +1,11 @@
+import ring_theory.roots_of_unity
+
+open polynomial
+
+namespace is_primitive_root
+
+lemma mem_nth_roots {A : Type*} [comm_ring A] [is_domain A] {n : ℕ} {ζ : A}
+  (hζ : is_primitive_root ζ n) (hn : 0 < n) : ζ ∈ nth_roots_finset n A :=
+(mem_nth_roots_finset hn).2 hζ.pow_eq_one
+
+end is_primitive_root
