ad45e6637ce91967a7c3779e0eaaa34c14a83586
riccardobrasca <riccardo.brasca@gmail.com>
2022-10-06 14:23:16 +0200

    better name

--- src/ready_for_mathlib/roots_of_unity.lean
+++ src/ready_for_mathlib/roots_of_unity.lean
@@ -4,7 +4,7 @@ open polynomial
 
 namespace is_primitive_root
 
-lemma mem_nth_roots {A : Type*} [comm_ring A] [is_domain A] {n : ℕ} {ζ : A}
+lemma mem_nth_roots_finset {A : Type*} [comm_ring A] [is_domain A] {n : ℕ} {ζ : A}
   (hζ : is_primitive_root ζ n) (hn : 0 < n) : ζ ∈ nth_roots_finset n A :=
 (mem_nth_roots_finset hn).2 hζ.pow_eq_one
 
