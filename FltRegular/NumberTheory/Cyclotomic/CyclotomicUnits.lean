e8835f236370ef2fb4defdc90c4efb0d24d390f3
Riccardo Brasca <riccardo.brasca@gmail.com>
2023-07-17 16:37:09 +0200

    done

--- FltRegular/NumberTheory/Cyclotomic/CyclotomicUnits.lean
+++ FltRegular/NumberTheory/Cyclotomic/CyclotomicUnits.lean
@@ -61,17 +61,17 @@ theorem associated_one_sub_pow_primitive_root_of_coprime {n j k : ℕ} {ζ : A}
   suffices ∀ {j : ℕ}, j.coprime n → Associated (1 - ζ) (1 - ζ ^ j) by
     exact (this hj).symm.trans (this hk)
   clear k j hk hj
-  refine' fun j hj =>
-    associated_of_dvd_dvd ⟨∑ i in range j, ζ ^ i, by rw [← geom_sum_mul_neg, mul_comm]⟩ _
+  intro j hj
+  refine' associated_of_dvd_dvd ⟨∑ i in range j, ζ ^ i, by rw [← geom_sum_mul_neg, mul_comm]⟩ _
   -- is there an easier way to do this?
   rcases eq_or_ne n 0 with (rfl | hn')
   · simp [j.coprime_zero_right.mp hj]
   rcases eq_or_ne n 1 with (rfl | hn)
-  · simp [is_primitive_root.one_right_iff.mp hζ]
-  replace hn := n.one_lt_of_ne hn' hn
+  · simp [IsPrimitiveRoot.one_right_iff.mp hζ]
+  replace hn := Nat.one_lt_of_ne n hn' hn
   obtain ⟨m, hm⟩ := Nat.exists_mul_emod_eq_one_of_coprime hj hn
   use ∑ i in range m, (ζ ^ j) ^ i
-  have : ζ = (ζ ^ j) ^ m := by rw [← pow_mul, pow_eq_mod_orderOf, ← hζ.eq_order_of, hm, pow_one]
+  have : ζ = (ζ ^ j) ^ m := by rw [← pow_mul, pow_eq_mod_orderOf, ← hζ.eq_orderOf, hm, pow_one]
   nth_rw 1 [this]
   rw [← geom_sum_mul_neg, mul_comm]
 
@@ -80,24 +80,23 @@ theorem IsPrimitiveRoot.sum_pow_unit {n k : ℕ} {ζ : A} (hn : 2 ≤ n) (hk : k
   by
   have h1 : (1 : ℕ).coprime n := Nat.coprime_one_left n
   have := associated_one_sub_pow_primitive_root_of_coprime _ hζ hk h1
-  simp at this 
-  rw [Associated] at this 
+  simp at this
+  rw [Associated] at this
   have h2 := mul_neg_geom_sum ζ k
   obtain ⟨u, hu⟩ := this
-  have := u.is_unit
+  have := u.isUnit
   convert this
-  rw [← hu] at h2 
-  simp at h2 
-  cases h2
+  rw [← hu] at h2
+  simp at h2
+  cases' h2 with h2 h2
   exact h2
   exfalso
   have hn1 : 1 < n := by linarith
   have hp := IsPrimitiveRoot.pow_ne_one_of_pos_of_lt hζ one_pos hn1
   simp at *
-  rw [sub_eq_zero] at h2 
-  rw [← h2] at hp 
-  simp only [eq_self_iff_true, not_true] at hp 
-  exact hp
+  rw [sub_eq_zero] at h2
+  rw [← h2] at hp
+  simp only [eq_self_iff_true, not_true] at hp
 
 theorem IsPrimitiveRoot.zeta_pow_sub_eq_unit_zeta_sub_one {p i j : ℕ} {ζ : A} (hn : 2 ≤ p)
     (hp : p.Prime) (hi : i < p) (hj : j < p) (hij : i ≠ j) (hζ : IsPrimitiveRoot ζ p) :
@@ -116,18 +115,18 @@ theorem IsPrimitiveRoot.zeta_pow_sub_eq_unit_zeta_sub_one {p i j : ℕ} {ζ : A}
     by_cases hj : 0 < j
     apply lt_trans _ hi
     apply Nat.sub_lt_of_pos_le _ _ hj hilj.le
-    simp only [not_lt, _root_.le_zero_iff] at hj 
+    simp only [not_lt, _root_.le_zero_iff] at hj
     rw [hj]
     simp only [tsub_zero]
     exact hi
   have h3 : IsUnit (-ζ ^ j * ∑ k : ℕ in range (i - j), ζ ^ (k : ℕ)) :=
     by
-    apply IsUnit.mul _ (is_primitive_root.sum_pow_unit _ hn hic hζ); apply IsUnit.neg
-    apply IsUnit.pow; apply hζ.is_unit hp.pos
+    apply IsUnit.mul _ (IsPrimitiveRoot.sum_pow_unit _ hn hic hζ); apply IsUnit.neg
+    apply IsUnit.pow; apply hζ.isUnit hp.pos
   obtain ⟨v, hv⟩ := h3
   use v
   rw [hv]
-  rw [mul_comm] at h2 
+  rw [mul_comm] at h2
   rw [mul_assoc]
   rw [h2]
   ring
@@ -143,18 +142,18 @@ theorem IsPrimitiveRoot.zeta_pow_sub_eq_unit_zeta_sub_one {p i j : ℕ} {ζ : A}
     by_cases hii : 0 < i
     apply lt_trans _ hj
     apply Nat.sub_lt_of_pos_le _ _ hii hilj
-    simp only [not_lt, _root_.le_zero_iff] at hii 
+    simp only [not_lt, _root_.le_zero_iff] at hii
     rw [hii]
     simp only [tsub_zero]
     exact hj
   have h3 : IsUnit (ζ ^ i * ∑ k : ℕ in range (j - i), ζ ^ (k : ℕ)) :=
     by
-    apply IsUnit.mul _ (is_primitive_root.sum_pow_unit _ hn hjc hζ); apply IsUnit.pow
-    apply hζ.is_unit hp.pos
+    apply IsUnit.mul _ (IsPrimitiveRoot.sum_pow_unit _ hn hjc hζ); apply IsUnit.pow
+    apply hζ.isUnit hp.pos
   obtain ⟨v, hv⟩ := h3
   use v
   rw [hv]
-  rw [mul_comm] at h2 
+  rw [mul_comm] at h2
   rw [mul_assoc]
   rw [h2]
 
@@ -201,4 +200,3 @@ end CyclotomicUnit
 end CyclotomicUnit
 
 end IsCyclotomicExtension
-
