6543a1e7e29331aa6a37339c8e85b80141122a22
Riccardo Brasca <riccardo.brasca@gmail.com>
2023-07-17 16:14:13 +0200

    fix this

--- FltRegular/NumberTheory/Cyclotomic/Factoring.lean
+++ FltRegular/NumberTheory/Cyclotomic/Factoring.lean
@@ -23,20 +23,20 @@ theorem pow_sub_pow_eq_prod_sub_zeta_runity_mul {K : Type _} [CommRing K] [IsDom
   -- suffices to show the identity in a multivariate polynomial ring with two generators over K
   suffices
     (X 0 : MvPolynomial (Fin 2) K) ^ (n : ℕ) - X 1 ^ (n : ℕ) =
-      ∏ ζ in nth_roots_finset n K, (X 0 - C ζ * X 1)
+      ∏ ζ in nthRootsFinset n K, (X 0 - MvPolynomial.C ζ * X 1)
     by
-    apply_fun MvPolynomial.eval fun i : Fin 2 => if i = 0 then x else y at this 
+    apply_fun MvPolynomial.eval fun i : Fin 2 => if i = 0 then x else y at this
     simpa [MvPolynomial.eval_prod] using this
   -- we know the univariate / one argument version of the identity
   have := X_pow_sub_one_eq_prod hpos h
   -- transfer this to a polynomial ring with two variables
-  have := congr_arg (aeval (X 0 : MvPolynomial (Fin 2) K)) this
+  have := congr_arg (Polynomial.aeval (X 0 : MvPolynomial (Fin 2) K)) this
   simp only [map_prod, aeval_X_pow, Polynomial.aeval_X, aeval_one, Polynomial.aeval_C,
-    AlgHom.map_sub] at this 
+    AlgHom.map_sub] at this
   -- the homogenization of the identity is also true
   have := congr_arg (homogenization 1) this
   -- simplify to get the result we want
-  simpa [homogenization_prod, algebra_map_eq, hpos]
+  simpa [homogenization_prod, algebraMap_eq, hpos]
 
 /-- If there is a primitive `n`th root of unity in `K` and `n` is odd, then
 `X ^ n + Y ^ n = ∏ (X + μ Y)`, where `μ` varies over the `n`-th roots of unity. -/
@@ -44,8 +44,7 @@ theorem pow_add_pow_eq_prod_add_zeta_runity_mul {K : Type _} [CommRing K] [IsDom
     {n : ℕ} (hodd : n % 2 = 1) (h : IsPrimitiveRoot ζ n) (x y : K) :
     x ^ (n : ℕ) + y ^ (n : ℕ) = ∏ ζ : K in nthRootsFinset n K, (x + ζ * y) :=
   by
-  have := pow_sub_pow_eq_prod_sub_zeta_runity_mul (nat.odd_iff.mpr hodd).Pos h x (-y)
-  simp only [mul_neg, sub_neg_eq_add] at this 
-  rw [neg_pow, neg_one_pow_eq_pow_mod_two] at this 
+  have := pow_sub_pow_eq_prod_sub_zeta_runity_mul (Nat.odd_iff.mpr hodd).pos h x (-y)
+  simp only [mul_neg, sub_neg_eq_add] at this
+  rw [neg_pow, neg_one_pow_eq_pow_mod_two] at this
   simpa [hodd] using this
-
