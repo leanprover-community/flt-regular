90c4e2b67760a1576d1c9c1ecd409118fda5a171
Ruben Van de Velde <65514131+Ruben-VandeVelde@users.noreply.github.com>
2023-09-12 20:42:10 +0200

    wip

--- lakefile.lean
+++ lakefile.lean
@@ -7,6 +7,8 @@ package «flt-regular» {
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
+require modularforms from git
+  "https://github.com/CBirkbeck/ModularForms_Lean4.git"
 
 @[default_target]
 lean_lib «FltRegular» {
