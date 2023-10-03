edeb7aafb28bf3b3f322e20a05334f94ec9f1335
Andrew Yang <36414270+erdOne@users.noreply.github.com>
2023-10-03 20:09:27 +0800

    Update KummersLemma.lean
    fix statement & deduplicate comment

--- FltRegular/NumberTheory/KummersLemma.lean
+++ FltRegular/NumberTheory/KummersLemma.lean
@@ -1,16 +1,13 @@
 import FltRegular.NumberTheory.RegularPrimes
 import Mathlib.NumberTheory.Cyclotomic.Rat
 
--- Let 𝑝 be a regular prime (i.e. an odd prime which does not divide the class number of
--- Let 𝑝 be a regular prime (i.e. an odd prime which does not divide the class number of
+open scoped NumberField
+
+-- Let 𝑝 be a regular prime (i.e. an odd prime which does not divide the class number off
 -- the 𝑝-th cyclotomic field) and 𝜉 a primitive 𝑝-th root of unity;
--- the 𝑝-th cyclotomic field) and 𝜉 a primitive 𝑝-th root of unity;
--- if a unit 𝑢∈𝐐(𝜉) is congruent to an integer modulo 𝑝, then 𝑢 is a 𝑝-th power in 𝐐(𝜉).
 -- if a unit 𝑢∈𝐐(𝜉) is congruent to an integer modulo 𝑝, then 𝑢 is a 𝑝-th power in 𝐐(𝜉).
 variable {K : Type*} {p : ℕ+} [hpri : Fact p.Prime] [Field K] [NumberField K] [IsCyclotomicExtension {p} ℚ K]
 variable (hp : p ≠ 2) [Fintype (ClassGroup (𝓞 K))] (hreg : (p : ℕ).Coprime <| Fintype.card <| ClassGroup (𝓞 K))
-
-open scoped NumberField
 
 theorem eq_pow_prime_of_unit_of_congruent (u : (𝓞 K)ˣ)
     (hcong : ∃ n : ℤ, ↑p ∣ (↑u : 𝓞 K) - n) : ∃ v, u = v ^ (p : ℕ) :=
