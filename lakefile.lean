58c147f56142d161f74c8b9be4a78410a8189a12
Riccardo Brasca <riccardo.brasca@gmail.com>
2023-10-23 17:31:46 +0200

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -8,6 +8,9 @@ package «flt-regular» {
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
+
 @[default_target]
 lean_lib «FltRegular» {
   -- add any library configuration options here
