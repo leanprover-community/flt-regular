1c37d409e1ad77f102351e3d7f4a565d62d97118
Riccardo Brasca <riccardo.brasca@gmail.com>
2025-09-17 15:47:12 +0200

    bump

--- FltRegular/CaseI/Statement.lean
+++ FltRegular/CaseI/Statement.lean
@@ -104,7 +104,7 @@ theorem auxf' (hp5 : 5 ≤ p) (a b : ℤ) (k₁ k₂ : Fin p) :
   have hs : s ⊆ range p := insert_subset_iff.2 ⟨mem_range.2 h0, insert_subset_iff.2
     ⟨mem_range.2 h1, insert_subset_iff.2 ⟨mem_range.2 (Fin.is_lt _),
     singleton_subset_iff.2 (mem_range.2 (Fin.is_lt _))⟩⟩⟩
-  have hcard := card_sdiff hs
+  have hcard := card_sdiff_of_subset hs
   replace hcard : (range p \ s).Nonempty := by
     rw [← Finset.card_pos, hcard, card_range]
     exact Nat.sub_pos_of_lt (lt_of_lt_of_le this hp5)
