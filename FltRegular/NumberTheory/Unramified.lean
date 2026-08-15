03c06d1b1186b7e1965d6419001a4329411bc470
Riccardo Brasca <riccardo.brasca@gmail.com>
2026-08-15 09:52:17 +0200

    more golfing

--- FltRegular/NumberTheory/Unramified.lean
+++ FltRegular/NumberTheory/Unramified.lean
@@ -3,6 +3,7 @@ module
 public import Mathlib.RingTheory.IntegralClosure.IntegralRestrict
 import Mathlib.NumberTheory.RamificationInertia.Galois
 import Mathlib.RingTheory.DedekindDomain.Different
+import Mathlib.RingTheory.DedekindDomain.Factorization
 public import Mathlib.RingTheory.DedekindDomain.Ideal.Lemmas
 public import Mathlib.RingTheory.Unramified.Locus
 
@@ -27,24 +28,6 @@ variable (R K L S : Type*) [CommRing R] [CommRing S] [Algebra R S] [Field K] [Fi
     [IsIntegralClosure S R L] [FiniteDimensional K L]
 
 variable {R} {S}
-
-lemma prod_primesOverFinset_of_unramified [Algebra.Unramified R S] [IsDedekindDomain S]
-    [Module.IsTorsionFree R S] (p : Ideal R) [p.IsPrime] (hp : p ≠ ⊥) :
-    ∏ P ∈ IsDedekindDomain.primesOverFinset p S, P = p.map (algebraMap R S) := by
-  have hpbot' : p.map (algebraMap R S) ≠ ⊥ := (Ideal.map_eq_bot_iff_of_injective
-      (Module.isTorsionFree_iff_algebraMap_injective.mp inferInstance)).not.mpr hp
-  rw [← associated_iff_eq.mp (factors_pow_count_prod hpbot')]
-  apply Finset.prod_congr rfl
-  intros P hP
-  convert (pow_one _).symm
-  have : p.IsMaximal := Ring.DimensionLEOne.maximalOfPrime hp ‹_›
-  rw [← Finset.mem_coe, IsDedekindDomain.coe_primesOverFinset hp] at hP
-  rw [← Ideal.IsDedekindDomain.ramificationIdx'_eq_factors_count hpbot' hP.1
-    (ne_bot_of_mem_primesOver hp hP)]
-  have : P.IsPrime := hP.1
-  have : P.LiesOver p := hP.2
-  rw [Ideal.ramificationIdx'_eq_ramificationIdx p P hp]
-  exact Ideal.ramificationIdx_eq_one P R
 
 lemma comap_map_eq_of_unramified [IsGalois K L] [Algebra.Unramified R S] (I : Ideal S)
     (hI : ∀ σ : L ≃ₐ[K] L, I.comap (galRestrict R K L S σ) = I) :
@@ -79,14 +62,13 @@ lemma comap_map_eq_of_unramified [IsGalois K L] [Algebra.Unramified R S] (I : Id
     simp only [factors_eq_normalizedFactors, Multiset.mem_toFinset,
       Ideal.mem_normalizedFactors_iff hIbot'] at hp
     have hpbot : p ≠ ⊥ := fun hp' ↦ hIbot' (eq_bot_iff.mpr (hp.2.trans_eq hp'))
-    have := hp.1
-    rw [← prod_primesOverFinset_of_unramified p hpbot, ← Finset.prod_pow]
+    have : p.IsPrime := hp.1
     have : p.IsMaximal := Ring.DimensionLEOne.maximalOfPrime hpbot this
+    rw [Ideal.map_algebraMap_eq_finsetProd_pow hpbot, ← Finset.prod_pow]
     apply Finset.prod_congr
     · ext P
       rw [factors_eq_normalizedFactors, Finset.mem_filter, Multiset.mem_toFinset,
-        Ideal.mem_normalizedFactors_iff hIbot, ← Finset.mem_coe,
-          IsDedekindDomain.coe_primesOverFinset hpbot S]
+        Ideal.mem_normalizedFactors_iff hIbot, Set.mem_toFinset]
       refine ⟨fun H ↦ ⟨H.1.1, ⟨H.2.symm⟩⟩, fun H ↦ ⟨⟨H.1, ?_⟩, ?_⟩⟩
       · have ⟨σ, hσ⟩ := exists_comap_galRestrict_eq R K L S (h𝔓' _ hp) H
         rw [← hσ, ← hI σ]
@@ -95,7 +77,10 @@ lemma comap_map_eq_of_unramified [IsGalois K L] [Algebra.Unramified R S] (I : Id
         rw [Ideal.under_def] at this
         exact this.symm
     · intro P hP
-      rw [← Finset.mem_coe, IsDedekindDomain.coe_primesOverFinset hpbot S] at hP
+      rw [Set.mem_toFinset] at hP
+      have : P.IsPrime := hP.1
+      have : P.LiesOver p := hP.2
+      rw [Ideal.ramificationIdx_eq_one P R, pow_one]
       congr
       rw [dite_eq_left hp, ← Nat.cast_inj (R := ENat), ← normalize_eq P,
         factors_eq_normalizedFactors,
@@ -159,13 +144,13 @@ lemma isUnramifiedAt_of_Separable_minpoly' [Algebra.IsSeparable K L]
   have hpbot : p ≠ ⊥ := Ideal.under_ne_bot R hPbot
   have : p.IsMaximal := (show p.IsPrime from inferInstance).isMaximal hpbot
   have hle : p ≤ P.comap (algebraMap R S) := Localization.le_comap_primeCompl_iff.mp fun _ a ↦ a
-  have := (separable_map (Ideal.quotientMap P (algebraMap R S) hle)).mpr h
-  rw [Polynomial.map_map, Ideal.quotientMap_comp_mk] at this
-  obtain ⟨a, b, e⟩ := this
-  apply_fun (aeval (Ideal.Quotient.mk P x)) at e
-  simp_rw [← Ideal.Quotient.algebraMap_eq, ← Polynomial.map_map, derivative_map, map_add,
-    _root_.map_mul, aeval_map_algebraMap, aeval_algebraMap_apply, minpoly.aeval, hxP, map_zero,
-    mul_zero, zero_add, map_one, zero_ne_one] at e
+  have hs := (separable_map (Ideal.quotientMap P (algebraMap R S) hle)).mpr h
+  rw [Polynomial.map_map, Ideal.quotientMap_comp_mk] at hs
+  apply hs.aeval_derivative_ne_zero (x := Ideal.Quotient.mk P x)
+  · rw [← Polynomial.map_map]
+    simp
+  · rw [derivative_map, ← Polynomial.map_map]
+    simpa [aeval_map_algebraMap] using hxP
 
 lemma isUnramifiedAt_of_Separable_minpoly [Algebra.IsSeparable K L]
     (P : Ideal S) [hP : P.IsPrime] (hPbot : P ≠ ⊥) (x : L) (hx : IsIntegral R x)
